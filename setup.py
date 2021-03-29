e76b34f63ec408f425afe81f17427a2dd8af6d2c
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-03-29 10:23:16 +0200

    Package Setup - setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="phoenix",
+    packages=find_packages(),
+    version="0.0.a",
+    description="Social media analysis and reporting.",
+    author="Build Up",
+    license="",
+)
+
