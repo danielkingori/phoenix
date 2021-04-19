7a8aa63e5f0ddbc244ca84bfd0197ddc0f42a9c3
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-04-19 09:53:02 +0200

    sc/fb_posts - artifaces init fix

--- phoenix/common/artifacts/__init__.py
+++ phoenix/common/artifacts/__init__.py
@@ -1,2 +1,4 @@
 """Init of artifacts package."""
+from phoenix.common.artifacts import dataframes  # noqa
+from phoenix.common.artifacts import json  # noqa
 from phoenix.common.artifacts import urls  # noqa
