a3097618c6a266145b7ce740122e05b9ee772063
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-04-23 14:29:15 +0200

    tag integration sample_messages

---
+++ tests/integration/tag/test_sample_messages.py
@@ -0,0 +1,24 @@
+"""Run the tagging on sample messages."""
+import pandas as pd
+import pytest
+
+from phoenix.common import utils
+
+
+@pytest.fixture
+def sample_messages_df():
+    """Get the sample message df."""
+    return pd.read_csv(utils.relative_path("./sample_messages.csv", __file__))
+
+
+def test_sample_messages(sample_messages_df):
+    """Run all tagging on the sample messages.
+
+    This will write "./output_tagging.csv" which
+    can then be checked by hand.
+    """
+    assert "message" in sample_messages_df
+
+    df = sample_messages_df.copy()
+
+    df.to_csv(utils.relative_path("./output_tagging.csv", __file__))
