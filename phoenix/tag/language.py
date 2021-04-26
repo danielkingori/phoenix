4e5b224ae1a45d703d1735afcddbd3d32834a771
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-04-26 15:31:53 +0200

    tag/lang-text-blob implement

--- phoenix/tag/language.py
+++ phoenix/tag/language.py
@@ -1,7 +1,14 @@
 """Language detection tagging."""
 import pandas as pd
+import textblob
 
 
 def execute(ser: pd.Series) -> pd.Series:
     """From a series of messages return the series of detect languages."""
-    return pd.Series(["en"] * len(ser))
+    return ser.apply(detect)
+
+
+def detect(message: str):
+    """Detect Language."""
+    tb = textblob.TextBlob(message)
+    return tb.detect_language()
