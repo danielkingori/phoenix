319d467317d0c1314abc0198cf582e1501b77945
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-04-26 15:37:41 +0200

    tag/lang-guess-language-spirit implement

--- phoenix/tag/language.py
+++ phoenix/tag/language.py
@@ -1,7 +1,13 @@
 """Language detection tagging."""
 import pandas as pd
+from guess_language import guess_language
 
 
 def execute(ser: pd.Series) -> pd.Series:
     """From a series of messages return the series of detect languages."""
-    return pd.Series(["en"] * len(ser))
+    return ser.apply(detect)
+
+
+def detect(message):
+    """Detect on the message."""
+    return guess_language(message)
