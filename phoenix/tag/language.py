2291f897f192aab490e7ac692bd92fea71a8d041
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-04-27 13:47:40 +0200

    tag/trans-google

--- phoenix/tag/language.py
+++ phoenix/tag/language.py
@@ -1,14 +1,18 @@
 """Language detection tagging."""
+import functools
+
 import pandas as pd
-import textblob
+from google.cloud import translate_v2 as translate
 
 
 def execute(ser: pd.Series) -> pd.Series:
     """From a series of messages return the series of detect languages."""
-    return ser.apply(detect)
+    translate_client = translate.Client()
+    curried_fn = functools.partial(translate_value, translate_client)
+    return ser.apply(curried_fn)
 
 
-def detect(message: str):
-    """Detect Language."""
-    tb = textblob.TextBlob(message)
-    return tb.detect_language()
+def translate_value(client, message: str):
+    """Translate."""
+    result = client.translate(message, target_language="ar")
+    return pd.Series([result["translatedText"], result["detectedSourceLanguage"]])
