c525b0b35760f5791b41ef89f78109eef480dfd3
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-04-29 16:30:32 +0200

    tag/lang-ployglot implement

--- phoenix/tag/language.py
+++ phoenix/tag/language.py
@@ -1,7 +1,14 @@
 """Language detection tagging."""
+from polyglot.detect import Detector
 import pandas as pd
 
 
-def execute(ser: pd.Series) -> pd.Series:
+def execute(ser: pd.Series):
     """From a series of messages return the series of detect languages."""
-    return pd.Series(["en"] * len(ser))
+    return ser.apply(detect)
+
+
+def detect(message: str):
+    """Detect for a message."""
+    ld = Detector(message)
+    return pd.Series([ld.language.code, ld.language.confidence])
