27aad28e3abe83ad7cee2b9a6fcd6f42402ee34e
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-05-20 16:06:36 +0200

    tag/basic-notebook todo on the tag

--- phoenix/tag/tag.py
+++ phoenix/tag/tag.py
@@ -10,5 +10,11 @@ PROCESSOR_NAME = "tag"
 def tag_dataframe(given_df: pd.DataFrame, message_key: str = "message") -> pd.DataFrame:
     """Tag Data."""
     df = given_df.copy()
+    # TODO: general clean message: `cleanMessage`
     df[["language", "confidence"]] = language.execute(df[message_key])
+    # TODO: arabicMessage (translation): `arabicMessage`
+    # TODO: feature extraction: `features`
+    # TODO: sentiment: `sentiment`
+    # TODO: topics: `topics`
+    # TODO: has key feature: `hasKeyFeature`
     return df
