75f5d689f77969212bf4ae893546072a417c54d3
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-05-26 07:48:53 +0200

    core/artifact_url_registry ArtifactURLRegistry

--- phoenix/common/artifacts/registry.py
+++ phoenix/common/artifacts/registry.py
@@ -1,6 +1,8 @@
 """Artifact Registry."""
 from typing import Any, Dict
 
+import datetime
+
 from phoenix.common.artifacts import urls
 
 
@@ -31,3 +33,37 @@ def source_url(
             return f"{prefix}{url_str}"
 
     raise ValueError(f"No url for artifact key: {artifact_key}")
+
+
+class ArtifactURLRegistry:
+    """Registry of the artifact urls."""
+
+    environment_key: str
+    run_datetime: datetime.datetime
+    mappers = {"source-": source_url}
+
+    def __init__(
+        self, run_datetime: datetime.datetime, environment_key: str = DEFAULT_ENVIRONMENT_KEY
+    ):
+        """Init ArtifactURLRegistry."""
+        self.environment_key = environment_key
+        self.run_datetime = run_datetime
+
+    def get_url(self, artifact_key: str, url_config: Dict[str, Any] = {}) -> str:
+        """Get the URL for the artifact key."""
+        url_config = self._build_url_config(url_config)
+        for mapper_prefix, url_fn in self.mappers.items():
+            if artifact_key.startswith(mapper_prefix):
+                return url_fn(artifact_key, url_config, self.environment_key)
+
+        raise ValueError(f"No url for artifact key: {artifact_key}")
+
+    def _build_url_config(self, url_config: Dict[str, Any]) -> Dict[str, Any]:
+        """Build the url config."""
+        if "RUN_ISO_TIMESTAMP" not in url_config:
+            url_config["RUN_ISO_TIMESTAMP"] = self.run_datetime.isoformat()
+
+        if "RUN_DATE" not in url_config:
+            url_config["RUN_DATE"] = self.run_datetime.strftime("%Y-%m-%d")
+
+        return url_config
