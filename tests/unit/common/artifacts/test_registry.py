396d6d0b3994b80241681634a818fa80ad3f0c30
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-05-26 07:48:53 +0200

    core/artifact_url_registry ArtifactURLRegistry

--- tests/unit/common/artifacts/test_registry.py
+++ tests/unit/common/artifacts/test_registry.py
@@ -1,5 +1,8 @@
 """Test Artifact Registry."""
+import datetime
+
 import mock
+import pytest
 
 from phoenix.common.artifacts import registry
 
@@ -16,3 +19,79 @@ def test_source_url(m_get_local):
     r_url = registry.source_url(artifact_key, url_config, environment_key)
 
     assert r_url == f"{local_path}RUN_DATE/source_runs/source-posts-RUN_ISO_TIMESTAMP.json"
+
+
+@mock.patch("phoenix.common.artifacts.registry.ArtifactURLRegistry._build_url_config")
+@mock.patch("phoenix.common.artifacts.registry.source_url")
+def test_artifact_url_registry(m_source_url, m_build_url_config):
+    """Test ArtifactURLRegistry."""
+    artifact_key = "source-posts"
+    run_datetime = datetime.datetime.now()
+    environment_key = "local"
+    url_config = {"RAN": "ran"}
+    aur = registry.ArtifactURLRegistry(run_datetime, environment_key)
+    aur.mappers["source-"] = m_source_url
+    r_url = aur.get_url(artifact_key, url_config)
+    m_build_url_config.assert_called_once_with(url_config)
+    m_source_url.assert_called_once_with(
+        artifact_key, m_build_url_config.return_value, environment_key
+    )
+
+    assert r_url == m_source_url.return_value
+
+
+@mock.patch("phoenix.common.artifacts.registry.ArtifactURLRegistry._build_url_config")
+@mock.patch("phoenix.common.artifacts.registry.source_url")
+def test_artifact_url_registry_value_error(m_source_url, m_build_url_config):
+    """Test ArtifactURLRegistry."""
+    artifact_key = "notsource-posts"
+    run_datetime = datetime.datetime.now()
+    environment_key = "local"
+    url_config = {"RAN": "ran"}
+    aur = registry.ArtifactURLRegistry(run_datetime, environment_key)
+    aur.mappers["source-"] = m_source_url
+    with pytest.raises(ValueError):
+        aur.get_url(artifact_key, url_config)
+        m_build_url_config.assert_called_once_with(url_config)
+        m_source_url.assert_not_called()
+
+
+def test_artiface_build_url_config_default():
+    """Test ArtifactURLRegistry._build_url_config."""
+    url_config = {"RAN": "ran"}
+    run_datetime = datetime.datetime.now()
+    environment_key = "local"
+    aur = registry.ArtifactURLRegistry(run_datetime, environment_key)
+    r_url_config = aur._build_url_config(url_config)
+    assert r_url_config == {
+        **url_config,
+        **{
+            "RUN_DATE": run_datetime.strftime("%Y-%m-%d"),
+            "RUN_ISO_TIMESTAMP": run_datetime.isoformat(),
+        },
+    }
+
+
+def test_artiface_build_url_config_non_default():
+    """Test ArtifactURLRegistry._build_url_config."""
+    url_config = {"RAN": "ran", "RUN_DATE": "RUN_DATE"}
+    run_datetime = datetime.datetime.now()
+    environment_key = "local"
+    aur = registry.ArtifactURLRegistry(run_datetime, environment_key)
+    r_url_config = aur._build_url_config(url_config)
+    assert r_url_config == {
+        **url_config,
+        **{
+            "RUN_ISO_TIMESTAMP": run_datetime.isoformat(),
+        },
+    }
+
+
+def test_artiface_build_url_config_non_default_all():
+    """Test ArtifactURLRegistry._build_url_config."""
+    url_config = {"RAN": "ran", "RUN_DATE": "RUN_DATE", "RUN_ISO_TIMESTAMP": "RUN_ISO_TIMESTAMP"}
+    run_datetime = datetime.datetime.now()
+    environment_key = "local"
+    aur = registry.ArtifactURLRegistry(run_datetime, environment_key)
+    r_url_config = aur._build_url_config(url_config)
+    assert r_url_config == url_config
