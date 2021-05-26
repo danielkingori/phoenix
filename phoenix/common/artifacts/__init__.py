612e30744fd35ddd4e303c3abc6445a07c9b22aa
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-05-26 08:11:24 +0200

    core/artifact_url_registry artifacts.registry

--- phoenix/common/artifacts/__init__.py
+++ phoenix/common/artifacts/__init__.py
@@ -1,4 +1,5 @@
 """Init of artifacts package."""
 from phoenix.common.artifacts import dataframes  # noqa
 from phoenix.common.artifacts import json  # noqa
+from phoenix.common.artifacts import registry  # noqa
 from phoenix.common.artifacts import urls  # noqa
