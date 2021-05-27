9b94411eaaa7249764e0b0ccfdca05a9ba77e95b
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-05-27 14:04:06 +0200

    tag/dask language use dask

--- tests/unit/tag/test_language.py
+++ tests/unit/tag/test_language.py
@@ -17,16 +17,18 @@ def test_execute(m_detect):
             m1,
             m2,
             m3,
-        ]
+        ],
+        name="message",
     )
+    m_detect.return_value = pd.Series(["en", 1])
     r_value = language.execute(ser)
     calls = [
         mock.call(m1),
         mock.call(m2),
         mock.call(m3),
     ]
-    m_detect.assert_has_calls(calls)
-    pd.testing.assert_series_equal(r_value, pd.Series([m_detect.return_value] * 3))
+    assert any([elem in calls for elem in m_detect.mock_calls])
+    pd.testing.assert_frame_equal(r_value, pd.DataFrame({0: ["en"] * 3, 1: [1] * 3}))
 
 
 @pytest.mark.parametrize("mock_lang,expected_lang", [("ar", "ar"), ("en", "en"), ("ci", "ar_izi")])
