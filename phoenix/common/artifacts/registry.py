bc57ca3c103de1b720f2010cd322410b87de5845
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-06-04 09:59:47 +0200

    core/artifact_url_registry tweets urls

--- phoenix/common/artifacts/registry.py
+++ phoenix/common/artifacts/registry.py
@@ -12,9 +12,17 @@ DEFAULT_ENVIRONMENT_KEY = "local"
 
 
 ArifactKey = Literal[
+    # Facebook posts
     "source-posts",
     "source-fb_post_source_api_notebook",
     "base-to_process_posts",
+    # Tweets
+    "source-user_tweets",
+    "source-keyword_tweets",
+    "source-twitter_user_notebook",
+    "source-twitter_keyword_notebook",
+    "base-to_process_user_tweets",
+    "base-to_process_keyword_tweets",
 ]
 
 
@@ -54,6 +62,7 @@ def default_url_prefix(
 
 
 DEFAULT_MAPPERS: Dict[ArifactKey, ArtifactURLMapper] = {
+    # Facebook Posts
     "source-posts": partial(
         url_mapper, "source_runs/{RUN_DATE}/source-posts-{RUN_ISO_TIMESTAMP}.json"
     ),
@@ -61,6 +70,25 @@ DEFAULT_MAPPERS: Dict[ArifactKey, ArtifactURLMapper] = {
         url_mapper, "source_runs/{RUN_DATE}/fb_post_source_api-{RUN_ISO_TIMESTAMP}.ipynb"
     ),
     "base-to_process_posts": partial(url_mapper, "base/to_process/posts-{RUN_ISO_TIMESTAMP}.json"),
+    # Twitter Tweets
+    "source-user_tweets": partial(
+        url_mapper, "source_runs/{RUN_DATE}/source-user_tweets-{RUN_ISO_TIMESTAMP}.json"
+    ),
+    "source-keyword_tweets": partial(
+        url_mapper, "source_runs/{RUN_DATE}/source-keyword-{RUN_ISO_TIMESTAMP}.json"
+    ),
+    "source-twitter_user_notebook": partial(
+        url_mapper, "source_runs/{RUN_DATE}/twitter_user_timeline-{RUN_ISO_TIMESTAMP}.ipynb"
+    ),
+    "source-twitter_keyword_notebook": partial(
+        url_mapper, "source_runs/{RUN_DATE}/twitter_keyword_search-{RUN_ISO_TIMESTAMP}.ipynb"
+    ),
+    "base-to_process_user_tweets": partial(
+        url_mapper, "base/to_process/twitter/user_tweets-{RUN_ISO_TIMESTAMP}.json"
+    ),
+    "base-to_process_keyword_tweets": partial(
+        url_mapper, "base/to_process/twitter/keyword_tweets-{RUN_ISO_TIMESTAMP}.json"
+    ),
 }
 
 
