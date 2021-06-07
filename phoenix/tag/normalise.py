1fe5e0bf458814f9e508b0c16d396859c58376ea
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-06-07 10:47:04 +0200

    tag/may language_distribution

--- phoenix/tag/normalise.py
+++ phoenix/tag/normalise.py
@@ -15,3 +15,12 @@ def execute(given_df: pd.DataFrame, message_key: str = "message") -> pd.DataFram
 def clean_message(message_ser) -> pd.Series:
     """Clean message."""
     return message_ser.replace(to_replace=r"https?:\/\/.*[\r\n]*", value="", regex=True)
+
+
+def language_distribution(normalised_df) -> pd.DataFrame:
+    """Get a distribution of languages."""
+    lang_dist = normalised_df.groupby("language").agg(
+        {"confidence": ["min", "max", "median", "skew", "mean"], "clean_message": "count"}
+    )
+    lang_dist = lang_dist.rename(columns={"clean_message": "number_of_items"})
+    return lang_dist
