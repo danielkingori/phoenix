b69399044d333c23efc7e92d2e12b3d856292b87
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-06-07 12:57:22 +0200

    tag/may join

--- phoenix/tag/normalise.py
+++ phoenix/tag/normalise.py
@@ -24,3 +24,25 @@ def language_distribution(normalised_df) -> pd.DataFrame:
     )
     lang_dist = lang_dist.rename(columns={"clean_message": "number_of_items"})
     return lang_dist
+
+
+def join_fb_posts_tweets(posts_df, tweets_df) -> pd.DataFrame:
+    """Join the facebook posts with the tweets."""
+    tweets_formated = tweets_df[
+        ["id_str", "clean_message", "full_text", "features", "features_count"]
+    ]
+    tweets_formated = tweets_formated.rename(columns={"id_str": "object_id", "full_text": "text"})
+    tweets_formated["object_type"] = "tweets"
+    posts_formated = posts_df[
+        [
+            "facebook_id",
+            "clean_message",
+            "message",
+            "features",
+            "features_count",
+        ]
+    ]
+    posts_formated = posts_formated.rename(columns={"facebook_id": "object_id", "message": "text"})
+    posts_formated["object_type"] = "facebook_posts"
+
+    return pd.concat([tweets_formated, posts_formated], axis=0, ignore_index=True)
