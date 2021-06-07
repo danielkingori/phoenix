24193f52bdb4c58e7052cfde058689461ec5c5db
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-06-07 07:46:50 +0200

    tag/may __init__ modules

--- phoenix/tag/__init__.py
+++ phoenix/tag/__init__.py
@@ -1,1 +1,5 @@
 """Tag module."""
+from phoenix.tag import key_features  # noqa
+from phoenix.tag import language  # noqa
+from phoenix.tag import tag  # noqa
+from phoenix.tag import text_features_analyser  # noqa
