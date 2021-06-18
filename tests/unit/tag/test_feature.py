32ae0d9a7cab078e23ba81b0ff88882c40575f60
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-06-18 08:08:29 +0200

    tag/may_feature_test test_finalise

--- tests/unit/tag/test_feature.py
+++ tests/unit/tag/test_feature.py
@@ -90,3 +90,35 @@ def test_get_key_items(exploded_features_df, is_key_feature_s):
             index=pd.Index([1, 3], name="object_id"),
         ),
     )
+
+
+@mock.patch("phoenix.tag.feature.key_features")
+def test_finalise(m_key_features, exploded_features_df, is_key_feature_s):
+    """Test finalise."""
+    m_key_features.return_value = is_key_feature_s
+    all_items_df = pd.DataFrame(
+        {
+            "object_id": [1, 2, 3],
+            "text": ["1-text", "2-text", "3-text"],
+        }
+    )
+    items, key_items, features_has = feature.finalise(all_items_df, exploded_features_df)
+    m_key_features.assert_called_once()
+    e_items = pd.DataFrame(
+        {
+            "object_id": [1, 2, 3],
+            "text": ["1-text", "2-text", "3-text"],
+            "has_key_feature": [True, False, True],
+            "features": [["1-f1", "1-f2"], ["2-f1", "2-f2"], ["3-f1", "3-f2"]],
+            "features_count": [[2, 1], [1, 2], [3, 3]],
+        },
+    )
+    pd.testing.assert_frame_equal(items, e_items)
+    pd.testing.assert_frame_equal(key_items, e_items[e_items["has_key_feature"].isin([True])])
+    e_features_has = exploded_features_df.copy()
+    e_features_has["is_key_feature"] = is_key_feature_s
+    e_features_has["has_key_feature"] = pd.Series(
+        [True, True, False, False, True, True],
+        index=pd.Int64Index([1, 1, 2, 2, 3, 3], dtype="int64", name="object_id"),
+    )
+    pd.testing.assert_frame_equal(features_has, e_features_has)
