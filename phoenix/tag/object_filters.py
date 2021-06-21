3f14e2e1c649ab46636e3a6dce6dce54df4ec24e
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-06-21 10:35:14 +0200

    tag/may_retweets object_filters get_key*

--- phoenix/tag/object_filters.py
+++ phoenix/tag/object_filters.py
@@ -1,4 +1,6 @@
 """Filters for objects."""
+import pandas as pd
+
 from phoenix.tag.data_pull import constants
 
 
@@ -20,3 +22,46 @@ def key_feature(df):
 def not_retweet(df):
     """Not retweet."""
     return ~df["is_retweet"]
+
+
+def get_key_facebook_posts(df):
+    """Filter key facebook posts."""
+    return df[(facebook_posts(df)) & (key_feature(df))]
+
+
+def get_key_tweets(df):
+    """Key tweets."""
+    return df[(tweets(df)) & (key_feature(df)) & (not_retweet(df))]
+
+
+def get_all_key_objects(df):
+    """All key objects.
+
+    This is used for determining the `is_key_object`.
+    """
+    return pd.concat([get_key_tweets(df), get_key_facebook_posts(df)])
+
+
+def is_key_object(df):
+    """Is key object."""
+    return df["is_key_object"].isin([True])
+
+
+def export(df, export_type):
+    """Filter dataframe for exporting."""
+    if "tweets" == export_type:
+        return df[(tweets(df))]
+
+    if "facebook_posts" == export_type:
+        return df[(facebook_posts(df))]
+
+    if "key_facebook_posts" == export_type:
+        return df[(facebook_posts(df)) & (is_key_object(df))]
+
+    if "key_tweets" == export_type:
+        return df[(tweets(df)) & (is_key_object(df))]
+
+    if "key_objects" == export_type:
+        return df[(is_key_object(df))]
+
+    raise ValueError(f"Export Type not supported: {export_type}")
