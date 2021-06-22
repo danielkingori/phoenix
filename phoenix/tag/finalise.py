e10d84dddccbadee4c365a6303666e5834eb4360
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-06-22 13:41:16 +0200

    tag/may_comments facebook_comments_finalise

--- phoenix/tag/finalise.py
+++ phoenix/tag/finalise.py
@@ -9,6 +9,14 @@ def join_objects_to_facebook_posts(objects, facebook_posts):
     return facebook_posts.join(objects, rsuffix="_objects")
 
 
+def join_objects_to_facebook_comments(objects, facebook_comments):
+    """Join the objects to the facebook_comments."""
+    objects = objects.set_index("object_id")
+    facebook_comments["object_id"] = facebook_comments["id"].astype(str)
+    facebook_comments = facebook_comments.set_index("object_id")
+    return facebook_comments.join(objects, rsuffix="_objects")
+
+
 def join_objects_to_tweets(objects, tweets):
     """Join the objects to the tweets."""
     objects = objects.set_index("object_id")
