c8afebca00c0e1ee6d59008a4690020c7d5487d8
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-06-24 19:49:21 +0200

    tag/may_topic_single FILL TOPIC

--- phoenix/tag/topic/single_feature_match.py
+++ phoenix/tag/topic/single_feature_match.py
@@ -2,6 +2,9 @@
 import pandas as pd
 
 
+FILL_TOPIC = "other"
+
+
 def get_topics(topic_config, features_df) -> pd.DataFrame:
     """Get the topics.
 
@@ -15,10 +18,19 @@ def get_topics(topic_config, features_df) -> pd.DataFrame:
     features_indexed_df = features_df.set_index("object_id")
     topic_config_i = topic_config.set_index("features")
     topics_df = features_indexed_df.join(topic_config_i, on="features")
+    no_topic = topics_df[topics_df["topic"].isnull()]
     topics_df = topics_df[~topics_df["topic"].isnull()]
     topics_df = (
         topics_df.groupby(["object_id", "topic", "object_type"])
         .agg({"features": list})
         .rename(columns={"features": "matched_features"})
     )
-    return topics_df.reset_index()
+    # Adding the FILL_TOPIC to all the objects that don't have a topic
+    no_topic = no_topic[~no_topic.index.isin(topics_df.index.get_level_values(0))]
+    no_topic = no_topic.reset_index()
+    no_topic = no_topic[["object_id", "object_type"]].drop_duplicates()
+    no_topic["topic"] = FILL_TOPIC
+    no_topic["matched_features"] = None
+    topics_df = topics_df.reset_index()
+
+    return pd.concat([topics_df, no_topic], ignore_index=True)
