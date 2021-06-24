cbba27ad7aec6278249efcc343c7829c623f5aa4
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-06-24 11:26:27 +0200

    tag/may_topic_single refactor out topic config

---
+++ phoenix/tag/topic/constants.py
@@ -0,0 +1,3 @@
+"""Topic Constants."""
+
+DEFAULT_RAW_TOPIC_CONFIG = "single_feature_match_topic_config.csv"
--- phoenix/tag/topic/single_feature_match.py
+++ phoenix/tag/topic/single_feature_match.py
@@ -1,11 +1,5 @@
 """Single feature match for topic analysis."""
 import pandas as pd
-import tentaclio
-
-from phoenix.common import artifacts
-
-
-DEFAULT_RAW_TOPIC_CONFIG = "single_feature_match_topic_config.csv"
 
 
 def get_topics(topic_config, features_df) -> pd.DataFrame:
@@ -28,28 +22,3 @@ def get_topics(topic_config, features_df) -> pd.DataFrame:
         .rename(columns={"features": "matched_features"})
     )
     return topics_df.reset_index()
-
-
-def get_topic_config(config_url=None) -> pd.DataFrame:
-    """Get topic config dataframe."""
-    df = _get_raw_topic_config(config_url)
-    df["topic"] = df["topic"].str.lower()
-    df = df[~df["topic"].isin(["no tag", "other"])].dropna()
-    df["topic_list"] = df["topic"].str.split(",")
-    df_ex = (
-        df.explode("topic_list", ignore_index=True)
-        .drop("topic", axis=1)
-        .rename(columns={"topic_list": "topic"})
-    )
-    df_ex["topic"] = df_ex["topic"].str.strip()
-    return df_ex[["features", "topic"]]
-
-
-def _get_raw_topic_config(config_url=None) -> pd.DataFrame:
-    """Get the raw topic_config."""
-    if not config_url:
-        config_url = f"{artifacts.urls.get_static_config()}{DEFAULT_RAW_TOPIC_CONFIG}"
-
-    with tentaclio.open(config_url, "r") as fb:
-        df = pd.read_csv(fb)
-    return df
