e7c1cc0a16627e100942ff50cd2023a778d21e5d
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-06-24 11:57:26 +0200

    tag/may_topic_single commit_topic_config

--- phoenix/tag/topic/single_feature_match_topic_config.py
+++ phoenix/tag/topic/single_feature_match_topic_config.py
@@ -24,7 +24,7 @@ def get_topic_config(config_url=None) -> pd.DataFrame:
 def _get_raw_topic_config(config_url=None) -> pd.DataFrame:
     """Get the raw topic_config."""
     if not config_url:
-        config_url = f"{artifacts.urls.get_static_config()}{constants.DEFAULT_RAW_TOPIC_CONFIG}"
+        config_url = _default_config_url()
 
     with tentaclio.open(config_url, "r") as fb:
         df = pd.read_csv(fb)
@@ -40,3 +40,38 @@ def merge_new_topic_config(topic_config, new_topic_config) -> pd.DataFrame:
     n_df = n_df.drop_duplicates()
     n_df = n_df.reset_index(drop=True)
     return n_df
+
+
+def commit_topic_config(topic_config: pd.DataFrame, config_url=None) -> pd.DataFrame:
+    """Commit a topic config.
+
+    This will commit the topic config in the format
+    features, topic
+    "f1", "t1,t2"
+
+    As this is easier to work with when labelling by hand.
+
+    Returns:
+    Persisted dataframe.
+    """
+    if not config_url:
+        config_url = _default_config_url()
+    df = topic_config.groupby("features", as_index=False).agg(
+        {"topic": lambda x: ", ".join(sorted(x.dropna()))}
+    )
+    _persist_topic_config_csv(df, config_url)
+    return df
+
+
+def _persist_topic_config_csv(df, config_url):
+    """Persist the topic config as a csv.
+
+    Mockable function used when testing commit_topic_config.
+    """
+    with tentaclio.open(config_url, "w") as fb:
+        df.to_csv(fb)
+
+
+def _default_config_url() -> str:
+    """Default config url."""
+    return f"{artifacts.urls.get_static_config()}{constants.DEFAULT_RAW_TOPIC_CONFIG}"
