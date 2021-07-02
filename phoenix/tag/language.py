5968e4edb8e975912b26755643e2f0d847fc4673
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-07-02 09:46:01 +0200

    tag/may_v3 language.py docs

--- phoenix/tag/language.py
+++ phoenix/tag/language.py
@@ -8,14 +8,25 @@ import pandas as pd
 from polyglot.detect import Detector
 
 
-def execute(ser: pd.Series):
-    """From a series of messages return the series of detect languages."""
-    ddf = dd.from_pandas(ser, npartitions=30)  # Should have npartitions configured in envirnment
+def execute(series_of_text: pd.Series):
+    """From a series of text return the series of detect languages."""
+    ddf = dd.from_pandas(
+        series_of_text, npartitions=30
+    )  # TODO: Should have npartitions configured in envirnment
     return ddf.apply(detect, meta={0: str, 1: int}).compute()
 
 
-def detect(message: str):
-    """Detect for a message."""
+def detect(message: str) -> pd.Series:
+    """Detect for a message.
+
+    Arguments:
+        text message
+
+    Returns:
+        pd.Series with dtypes:
+            0: string,
+            1: int,
+    """
     code, confidence = detect_lang_code(message)
     if code not in ["en", "ar"]:
         code = "ar_izi"
@@ -30,7 +41,8 @@ def detect_lang_code(message: str) -> Tuple[str, float]:
     try:
         ld = Detector(message)
         return ld.language.code, ld.language.confidence
-    # TODO: fix this
+    # TODO: errors should be added as a new language type
+    # This will then clarify which texts can't be detected on
     except:  # noqa[E722]
         logging.info(f"Info: An exception the detection occured for message {message}")
         return "en", 0.0
