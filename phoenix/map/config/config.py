635928e8f29c19e53d1b77a6efcf1987534537c4
wunderwood <william.james.underwood@gmail.com>
2021-07-03 10:22:05 +0100

    model-0001 - linting fixes

--- phoenix/map/config/config.py
+++ phoenix/map/config/config.py
@@ -1,11 +1,14 @@
-import os
-import yaml
+"""config.py."""
 from typing import Any, Dict, List, Optional
+
+import os
+
+import yaml
 
 
 def get_map_config(data_origin: str) -> List[Dict[str, Optional[Any]]]:
-    """Gets list of config dicts for each dataset that needs to be
-    extracted from source data files.
+    """Gets list of config dicts for each dataset that needs mapping.
+
     param data_origin: a code representing the social platform
         the raw data was sourced from E.G. fb | tw
     type data_origin: str
@@ -13,39 +16,29 @@ def get_map_config(data_origin: str) -> List[Dict[str, Optional[Any]]]:
     rtype: List[Dict[str, Optional[Any]]]
     """
     # Get all job configs
-    with open(os.path.join(
-        os.path.dirname(os.path.abspath(__file__)),
-            'map_config.yml'
-    )) as config_file:
-        all_cfg = yaml.load(
-            config_file,
-            Loader=yaml.FullLoader
-        )
+    with open(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "map_config.yml")
+    ) as config_file:
+        all_cfg = yaml.load(config_file, Loader=yaml.FullLoader)
     # Filter for origin
-    config = [
-        cfg for key, cfg in all_cfg.items()
-        if cfg['data_origin'] == data_origin
-    ]
+    config = [cfg for key, cfg in all_cfg.items() if cfg["data_origin"] == data_origin]
     return config
 
 
 def get_env_config() -> Dict[str, Optional[Any]]:
     """Gets environment config for each env type [local_dev|docker_dev|docker_prod].
+
     return: Dict of environment config settings
     rtype: Dict[str, Optional[Any]]
     """
     # Get configs and set constants
-    if os.environ.get('IS_DOCKER_CONTAINER', False):
-        env_type = 'docker_dev'
+    if os.environ.get("IS_DOCKER_CONTAINER", False):
+        env_type = "docker_dev"
     else:
-        env_type = 'local_dev'
-    with open(os.path.join(
-        os.path.dirname(os.path.abspath(__file__)),
-            'env_config.yml'
-    )) as env_config_file:
-        all_env_cfg = yaml.load(
-            env_config_file,
-            Loader=yaml.FullLoader
-        )
+        env_type = "local_dev"
+    with open(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "env_config.yml")
+    ) as env_config_file:
+        all_env_cfg = yaml.load(env_config_file, Loader=yaml.FullLoader)
     env_config = all_env_cfg[env_type]
     return env_config
--- phoenix/map/data_mapper.py
+++ phoenix/map/data_mapper.py
@@ -1,19 +1,22 @@
 """data_mapper.py."""
-import logging
+from typing import Any, Dict, List, Optional
+
 import hashlib
 import json
-import pandas as pd
+import logging
 import os
 from datetime import datetime
-from typing import Any, Dict, List, Optional
+
+import pandas as pd
+
 from phoenix.common.artifacts import json as json_arti
 from phoenix.map.config import config
 
 
 # Logging config
 logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger('DataMapper')
-logging.getLogger('DataMapper').setLevel(logging.DEBUG)
+logger = logging.getLogger("DataMapper")
+logging.getLogger("DataMapper").setLevel(logging.DEBUG)
 
 
 class DataMapper:
@@ -48,24 +51,21 @@ class DataMapper:
         Moves processed raw post files to an archive dir
         so they dont get processed more than once
     """
-    SOURCE_FILE_SUFFIX = '.json'
+
+    SOURCE_FILE_SUFFIX = ".json"
 
     def __init__(self, data_origin: str):
         self.data_origin = data_origin
         self._env_config = config.get_env_config()
         self._map_config = config.get_map_config(data_origin=self.data_origin)
         self._batch_ts = datetime.now().strftime("%Y%m%d_%H%M%S")
-        self._data_dir_base = self._env_config['data_dir_base']
-        self._data_dir_idl = self._env_config['data_dir_idl']
-        self._data_dir_arch = self._env_config['data_dir_arch']
+        self._data_dir_base = self._env_config["data_dir_base"]
+        self._data_dir_idl = self._env_config["data_dir_idl"]
+        self._data_dir_arch = self._env_config["data_dir_arch"]
         self.expected_output_count = len(self._map_config)
         self.files_to_process = []
         self.output_files = []
-        self._make_required_dirs([
-            self._data_dir_base,
-            self._data_dir_idl,
-            self._data_dir_arch
-        ])
+        self._make_required_dirs([self._data_dir_base, self._data_dir_idl, self._data_dir_arch])
 
     @staticmethod
     def _normalize_nested_json(df: pd.DataFrame, cfg: Dict[str, Optional[Any]]) -> pd.DataFrame:
@@ -73,13 +73,14 @@ class DataMapper:
         json_obj = [
             {
                 # Add source file name to help calc curr in pg db
-                'src_file_name': row['src_file_name'],
+                "src_file_name": row["src_file_name"],
                 # Hash pk to use as pk|fk in pg db
-                cfg['foreign_key_field'] + '_hash': hashlib.md5(
-                    json.dumps(row[cfg['foreign_key_field']]).encode()
+                cfg["foreign_key_field"]
+                + "_hash": hashlib.md5(
+                    json.dumps(row[cfg["foreign_key_field"]]).encode()
                 ).hexdigest(),
                 # Add field containing array that needs normalising
-                cfg['normalize_field']: row[cfg['normalize_field']]
+                cfg["normalize_field"]: row[cfg["normalize_field"]],
             }
             for i, row in df.iterrows()
         ]
@@ -89,11 +90,11 @@ class DataMapper:
 
     @staticmethod
     def _reshape_df(df, map_cfg) -> pd.DataFrame:
-        logger.info('Reshaping dataframe to include full universe of columns.')
+        logger.info("Reshaping dataframe to include full universe of columns.")
         # Create list of arrived cols
         arrived_columns = [col for col in df.columns]
         # Combine arrived cols with all expected cols
-        all_columns = list(set(arrived_columns + map_cfg['columns']))
+        all_columns = list(set(arrived_columns + map_cfg["columns"]))
         # Create a template df with all required cols
         df_template = pd.DataFrame(columns=all_columns)
         # Reshape passed df to new df containing all expected cols
@@ -102,7 +103,7 @@ class DataMapper:
 
     @staticmethod
     def _make_required_dirs(lists_of_dirs: List) -> None:
-        logger.info('Creating required dirs if they don''t already exist.')
+        logger.info("Creating required dirs if they don" "t already exist.")
         for required_dir in lists_of_dirs:
             os.makedirs(os.path.join(*required_dir), exist_ok=True)
 
@@ -114,7 +115,7 @@ class DataMapper:
                 artifacts_json_url=os.path.join(*self._data_dir_base, file)
             ).obj
             artifact_df = pd.DataFrame(json_obj)
-            artifact_df['src_file_name'] = file
+            artifact_df["src_file_name"] = file
             df_list.append(artifact_df)
         # Concat df's together if running map job for multiple files
         loaded_df = pd.concat(objs=df_list, ignore_index=True, axis=0)
@@ -124,84 +125,87 @@ class DataMapper:
 
     def get_files_to_process(self) -> None:
         """Gets a list of files to process from the source incoming dir.
+
         return: None
         rtype: None
         """
-        logger.info(f"""
+        logger.info(
+            f"""
         Searching base dir {os.path.join(*self._data_dir_base)} for files to process.
-        """)
+        """
+        )
         # Get list of file names in base/to_process dir
         self.files_to_process = [
-            obj for obj in os.listdir(os.path.join(*self._data_dir_base))
+            obj
+            for obj in os.listdir(os.path.join(*self._data_dir_base))
             if str(obj).endswith(self.SOURCE_FILE_SUFFIX)
         ]
 
     def map_data_to_idl_files(self) -> None:
-        """Maps the raw post data to normalised json files ready to be
-        loaded to the postgres db.
+        """Maps raw post data to normalised json files.
+
         return: None
         rtype: None
         """
-        logger.info('Starting data map process.')
+        logger.info("Starting data map process.")
         # Loop for when multiple datasets are extracted from single source file
         for cfg in self._map_config:
             # Load data file(s) to single df
             posts_df = self._load_files_to_df(self.files_to_process, cfg)
             # Create new df of just cols we want
-            df = posts_df[cfg['columns']].copy()
+            df = posts_df[cfg["columns"]].copy()
             # Check for and run processing step(s)
-            if cfg['processing_steps']:
-                for process_step in cfg['processing_steps']:
+            if cfg["processing_steps"]:
+                for process_step in cfg["processing_steps"]:
                     logger.info(f"Processing step {process_step} for {cfg['name']}")
                     df = eval(f"self.{process_step}(df, cfg)")
             df = df.fillna("")
             # Persist idl df to idl json ready for pg db ingest
             persisted_object = json_arti.persist(
                 artifacts_json_url=os.path.join(
-                    *self._data_dir_idl,
-                    f"{cfg['name']}_{self._batch_ts}.json"
+                    *self._data_dir_idl, f"{cfg['name']}_{self._batch_ts}.json"
                 ),
-                obj=df.to_dict(orient='records')
+                obj=df.to_dict(orient="records"),
             )
             self.output_files.append(persisted_object.url)
 
     def archive_processed_files(self) -> None:
-        """Moves processed raw post files to an archive dir so they dont
-        get processed more than once.
+        """Move processed post files to an archive dir.
+
         return: None
         rtype: None
         """
-        logger.info('Starting data archive process.')
+        logger.info("Starting data archive process.")
         cwd_path = os.getcwd()
         for file in self.files_to_process:
             os.rename(
                 src=os.path.join(cwd_path, *self._data_dir_base, file),
-                dst=os.path.join(cwd_path, *self._data_dir_arch, file)
+                dst=os.path.join(cwd_path, *self._data_dir_arch, file),
             )
 
 
 def main() -> int:
     """Main program. Create and run data mapper."""
     # Instantiate data_mapper
-    data_mapper = DataMapper(data_origin='fb')
+    data_mapper = DataMapper(data_origin="fb")
     # Poll base dir for new files to process
     data_mapper.get_files_to_process()
     # If new files exist map base data to idl data
     if len(data_mapper.files_to_process) > 0:
-        logger.info('New files to process found.')
+        logger.info("New files to process found.")
         data_mapper.map_data_to_idl_files()
     else:
-        logger.info('No new files to process found.')
-        logger.info('Exiting data map process.')
+        logger.info("No new files to process found.")
+        logger.info("Exiting data map process.")
     # If correct number of files created archive processed files
     if len(data_mapper.output_files) == data_mapper.expected_output_count:
-        logger.info('Output file count is as expected.')
+        logger.info("Output file count is as expected.")
         data_mapper.archive_processed_files()
     else:
-        logger.info('Output file count is not as expected.')
-        logger.info('Exiting data archive process.')
+        logger.info("Output file count is not as expected.")
+        logger.info("Exiting data archive process.")
     return 0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
