8d65878ffcfc0a948b5c80d34d06ac9cc6a81123
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-07-05 11:29:19 +0200

    tag/aws_comprehend boilerplate module and notebook

---
+++ phoenix/tag/third_party_models/aws_comprehend.py
@@ -0,0 +1,14 @@
+"""AWS comprehend functionality."""
+import pandas as pd
+
+
+def execute(objects: pd.DataFrame) -> pd.DataFrame:
+    """Execute the aws comprehend models on the objects dataframe.
+
+    Arguments:
+        objects: objects dataframe. See docs/schemas/objects.md
+
+    Returns:
+        TODO
+    """
+    return pd.DataFrame({})
