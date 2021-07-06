a0dbecb8fa59dcde06aab3ffbf8c02b950b8bbc5
Tim Herfurth <timherfurth87@gmail.com>
2021-07-06 11:11:32 +0200

    v1 of AWS sentiment analysis

--- phoenix/tag/third_party_models/aws_comprehend.py
+++ phoenix/tag/third_party_models/aws_comprehend.py
@@ -1,14 +1,49 @@
 """AWS comprehend functionality."""
+import os
+
+import boto3
 import pandas as pd
 
 
-def execute(objects: pd.DataFrame) -> pd.DataFrame:
-    """Execute the aws comprehend models on the objects dataframe.
+AWS_ACCESS_KEY_ID = os.getenv("w")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+
+
+def sentiment_analysis(objects: pd.DataFrame) -> pd.DataFrame:
+    """Execute sentiment analysis via AWS comprehend models on the objects dataframe.
 
     Arguments:
         objects: objects dataframe. See docs/schemas/objects.md
 
     Returns:
-        TODO
+        dataframe with sentiment and sentiment score columns added (careful, different row order).
     """
-    return pd.DataFrame({})
+    # TODO: restriction is 5000 bytes per text; how to optimally ensure this?
+    objects["clean_text"] = objects["clean_text"].apply(lambda x: x[:1000])
+    objects["sentiment"] = pd.NA
+    objects["sentiment_scores"] = pd.NA
+
+    n = 25  # batch size (max=25)
+    comprehend = boto3.client(
+        "comprehend",
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    )
+
+    arabic = objects[objects["language"] == "ar"]
+    english = objects[objects["language"] == "en"]
+    ar_izi = objects[objects["language"] == "ar_izi"]
+
+    for df, lang in zip([arabic, english], ["ar", "en"]):
+        for i in range(0, len(df), n):
+            comprehend_response = comprehend.batch_detect_sentiment(
+                TextList=list(df["clean_text"].iloc[i : i + n]),
+                LanguageCode=lang,
+            )
+            response_df = pd.DataFrame(comprehend_response["ResultList"])
+            df["sentiment"].iloc[i : i + n] = response_df["Sentiment"].values
+            df["sentiment_scores"].iloc[i : i + n] = response_df["SentimentScore"].values
+
+    results_df = pd.concat([arabic, english, ar_izi])
+
+    return results_df
