25f0fff0ebe50f94c57bc03fd5573a98e0a41372
Tim Herfurth <timherfurth87@gmail.com>
2021-07-06 11:11:32 +0200

    v1 of AWS sentiment analysis

--- phoenix/tag/__init__.py
+++ phoenix/tag/__init__.py
@@ -3,3 +3,4 @@ from phoenix.tag import feature  # noqa
 from phoenix.tag import language  # noqa
 from phoenix.tag import normalise  # noqa
 from phoenix.tag import text_features_analyser  # noqa
+from phoenix.tag.third_party_models import aws_comprehend  # noqa
