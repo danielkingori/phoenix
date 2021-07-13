d43a5a4f1a84e16761d6ae72c5d1253dc389794f
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-07-13 11:58:26 +0200

    dm/facebook_json camel_to_snake

--- phoenix/tag/data_pull/utils.py
+++ phoenix/tag/data_pull/utils.py
@@ -13,3 +13,13 @@ def to_type(column_name: str, astype, df: pd.DataFrame):
 def words_to_snake(name: str):
     """Map Words with spaces to snake case."""
     return re.sub(r"\W+", "_", name.lower())
+
+
+def camel_to_snake(name):
+    """Camel case string to snake case.
+
+    Taken from:
+    https://stackoverflow.com/questions/1175208/elegant-python-function-to-convert-camelcase-to-snake-case
+    """
+    name = re.sub("(.)([A-Z][a-z]+)", r"\1_\2", name)
+    return re.sub("([a-z0-9])([A-Z])", r"\1_\2", name).lower()
