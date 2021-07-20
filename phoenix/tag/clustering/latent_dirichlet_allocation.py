2497724b93ffbf3e8cc2d8142b0e3855eb2ccb40
Andrew Sutjahjo <andrewsutjahjo@gmail.com>
2021-07-20 16:20:42 +0200

    Init clustering.latent_dirichlet_allocation

---
+++ phoenix/tag/clustering/latent_dirichlet_allocation.py
@@ -0,0 +1,9 @@
+"""Implements Latent Dirichlet Allocation on data."""
+
+
+class LatentDirichletAllocator:
+    """LatentDirichletAllocator.
+
+    Train LatentDirichletAllocation model to segment groups of objects' text (posts/tweets) and
+    explain which words in the texts were most important in the segmentation.
+    """
