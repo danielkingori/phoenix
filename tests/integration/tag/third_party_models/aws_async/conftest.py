a8bcfd78618635e2206b927c1bfe56d4677d1df8
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-09-02 13:06:05 +0200

    tag/language_sentiment test output data
    Includes:
    - fixtures for the urls
    - Test data that is from real sentiment analysis output

--- tests/integration/tag/third_party_models/aws_async/conftest.py
+++ tests/integration/tag/third_party_models/aws_async/conftest.py
@@ -2,6 +2,8 @@
 import pandas as pd
 import pytest
 
+from phoenix.common import utils
+
 
 @pytest.fixture
 def aws_sentiment_objects():
@@ -13,3 +15,45 @@ def aws_sentiment_objects():
             "language": ["ar", "ar", "ar", "en", "en"],
         }
     )
+
+
+@pytest.fixture
+def aws_sentiment_ar_output_url():
+    """Output URL for the test data of Arabic language.
+
+    This data creates test data that matches what aws comprehend outputs.
+    As defined here:
+    https://docs.aws.amazon.com/comprehend/latest/dg/how-async.html#async-sentiment
+    The line numbers are not ordered in the original and should not be in the test data.
+
+    To recreate the tarball use this comment from the phoenix root
+    directory:
+    ```
+    tar -czvf \
+        tests/integration/tag/third_party_models/aws_async/test_data/ar_output.tar.gz \
+        tests/integration/tag/third_party_models/aws_async/test_data/ar_output
+    ```
+    """
+    file_path = utils.relative_path("./test_data/ar_output.tar.gz", __file__)
+    return f"file:///{file_path}"
+
+
+@pytest.fixture
+def aws_sentiment_en_output_url():
+    """Output URL for the test data of Arabic language.
+
+    This data creates test data that matches what aws comprehend outputs.
+    As defined here:
+    https://docs.aws.amazon.com/comprehend/latest/dg/how-async.html#async-sentiment
+    The line numbers are not ordered in the original and should not be in the test data.
+
+    To recreate the tarball use this comment from the phoenix root
+    directory:
+    ```
+    tar -czvf \
+        tests/integration/tag/third_party_models/aws_async/test_data/en_output.tar.gz \
+        tests/integration/tag/third_party_models/aws_async/test_data/en_output
+    ```
+    """
+    file_path = utils.relative_path("./test_data/en_output.tar.gz", __file__)
+    return f"file:///{file_path}"
