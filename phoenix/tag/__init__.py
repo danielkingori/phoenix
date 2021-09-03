9dc154a39652d2860903c3fb81fe99878e736d45
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-09-03 07:44:28 +0200

    tag/language_sentiment remove of aws_comprehend

--- phoenix/tag/__init__.py
+++ phoenix/tag/__init__.py
@@ -3,4 +3,3 @@ from phoenix.tag import feature  # noqa
 from phoenix.tag import language  # noqa
 from phoenix.tag import normalise  # noqa
 from phoenix.tag import text_features_analyser  # noqa
-from phoenix.tag.third_party_models import aws_comprehend  # noqa
