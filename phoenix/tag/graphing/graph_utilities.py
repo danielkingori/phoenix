1446c0222ed9f94c3f17b25b5fb10e44c6f1bdc8
watermosaic <jacob@jacoblefton.com>
2021-09-07 22:41:37 +0200

    make fix

--- phoenix/tag/graphing/graph_utilities.py
+++ phoenix/tag/graphing/graph_utilities.py
@@ -3,6 +3,7 @@
 import networkx as nx
 import tentaclio
 
+
 def clean_n_edge_nodes(graph: nx.Graph, n: int = 1):
     """Remove nodes with n degree, default 1, for a cleaner graph."""
     to_be_removed = [x for x in graph.nodes() if graph.degree(x) <= n]
@@ -21,5 +22,5 @@ def get_partitions_set(partitions):
 
 def save_graph(graph, path):
     """Saves the webweb visualization to the specified path with Tentaclio."""
-    with tentaclio.open(path, 'w') as f:
+    with tentaclio.open(path, "w") as f:
         f.write(graph.html)
