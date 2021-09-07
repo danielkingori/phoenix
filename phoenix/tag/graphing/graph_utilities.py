10e460bcaf0bd8e9df988d2d0a82194a41ab7add
watermosaic <jacob@jacoblefton.com>
2021-09-07 21:37:02 +0200

    separate out graphing utilities

---
+++ phoenix/tag/graphing/graph_utilities.py
@@ -0,0 +1,19 @@
+"""Utilities for graphing."""
+
+import networkx as nx
+
+
+def clean_n_edge_nodes(graph: nx.Graph, n: int = 1):
+    """Remove nodes with n degree, default 1, for a cleaner graph."""
+    to_be_removed = [x for x in graph.nodes() if graph.degree(x) <= n]
+    for x in to_be_removed:
+        graph.remove_node(x)
+    return graph
+
+
+def get_partitions_set(partitions):
+    """Get a list of the different partitions."""
+    # This output would go to the webweb metadata for coloring,
+    #   but I was unable to make it work.
+    parts = [partitions[k] for k in partitions.keys()]
+    return list(set(parts))
