fbd8b292ff48d2327aedd27c4398b9ce53af32e6
watermosaic <jacob@jacoblefton.com>
2021-09-10 23:34:29 +0200

    facebook topic graph py and notebooks

---
+++ phoenix/tag/graphing/facebook_topics_graph.py
@@ -0,0 +1,32 @@
+"""Facebook topics network visualization."""
+
+import community as community_louvain
+import networkx as nx
+import pandas as pd
+
+
+from phoenix.tag.graphing import graph_utilities as graph_util
+from phoenix.tag.graphing import webweb_visualition_configuration as viz_config
+
+
+def create_networkx_graph_from_df(data):
+    """Create network graph from a dataframe."""
+    return nx.from_pandas_edgelist(data, "topic-1", "topic-2", edge_attr="weight")
+
+
+def assign_partitions(graph):
+    """Assigns partition to nodes using community-louvain package."""
+    # Define partitions
+    partitions = community_louvain.best_partition(graph)
+    # Set partitions in graph
+    nx.set_node_attributes(graph, partitions, "community")
+    return graph
+
+
+def generate_graph_viz(data: pd.DataFrame):
+    """Run the graph visualization process."""
+    graph = create_networkx_graph_from_df(data)
+    partitions = community_louvain.best_partition(graph)
+    community_graph = assign_partitions(graph)
+    web = viz_config.create_facebook_topic_to_topic_visualization(community_graph)
+    return web
