99f12350e20b0fcc0a2a3218173cd4c87a24f54a
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-09-17 09:23:08 +0200

    tag/model_finalise tag_tension.normalise

--- tests/unit/tag/tension/test_tag_tension.py
+++ tests/unit/tag/tension/test_tag_tension.py
@@ -1,5 +1,5 @@
 """Unit test for tag_tension."""
-
+import mock
 import pandas as pd
 import pytest
 
@@ -38,3 +38,45 @@ def test_tag_object_has_tension():
     output_df = tag_tension.tag_object_has_tension(input_df)
 
     pd.testing.assert_series_equal(output_df["has_tension"], expected_series)
+
+
+def test_normalise_tension_cols():
+    """Test normalise tension cols."""
+    input_df = pd.DataFrame(
+        {
+            "is_economic_labour_tension": [1, 0, 1, 0],
+            "is_sectarian_tension": [False, False, False, True],
+            "is_environmental_tension": [False] * 4,
+            "is_political_tension": [False] * 4,
+            "is_service_related_tension": [False] * 4,
+            "is_community_insecurity_tension": [False] * 4,
+            "not_a_tension": [True] * 4,
+        }
+    )
+    expected_df = pd.DataFrame(
+        {
+            "is_economic_labour_tension": [True, False, True, False],
+            "is_sectarian_tension": [False, False, False, True],
+            "is_environmental_tension": [False] * 4,
+            "is_political_tension": [False] * 4,
+            "is_service_related_tension": [False] * 4,
+            "is_community_insecurity_tension": [False] * 4,
+            "not_a_tension": [True] * 4,
+            "is_geopolitics_tension": [False] * 4,
+            "is_intercommunity_relations_tension": [False] * 4,
+        }
+    )
+
+    output_df = tag_tension.normalise_tension_cols(input_df)
+    pd.testing.assert_frame_equal(output_df, expected_df)
+
+
+@mock.patch("phoenix.tag.tension.tag_tension.normalise_tension_cols")
+@mock.patch("phoenix.tag.tension.tag_tension.tag_object_has_tension")
+def test_normalise(m_tag_object_has_tension, m_normalise_tension_cols):
+    """Test normalise."""
+    input_df = mock.Mock()
+    result = tag_tension.normalise(input_df)
+    m_normalise_tension_cols.assert_called_once_with(input_df)
+    m_tag_object_has_tension.assert_called_once_with(m_normalise_tension_cols.return_value)
+    assert m_tag_object_has_tension.return_value == result
