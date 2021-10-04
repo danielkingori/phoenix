fd52a7dc4e455c202cb6d3aae4da8304eb035f86
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-10-04 14:02:58 +0200

    core/cli_comments tag_phase_2

--- phoenix/common/cli_modules/comments.py
+++ phoenix/common/cli_modules/comments.py
@@ -107,3 +107,79 @@ def run_phase(
 
     start_offset = start_offset - 1
     tagging._run_tagging_notebooks(1, "facebook_comments", parameters, art_url_reg, start_offset)
+
+
+@comments.command(
+    "tag_phase_2",
+    context_settings=dict(
+        ignore_unknown_options=True,
+        allow_extra_args=True,
+    ),
+)
+@click.argument("month_offset", type=click.INT, default=0)
+@click.argument("artifact_env", default="local", envvar="ARTIFACT_ENV")
+@click.option(
+    "--start_offset",
+    default=0,
+    help=("Start notebook from offset."),
+)
+@click.option(
+    "--silence_no_files_to_process_exception",
+    default=False,
+    type=click.BOOL,
+    help=(
+        "Silence no files to process exception\n"
+        "This is for when the CLI is used in cron jobs so that unnecessary errors are created."
+    ),
+)
+@click.pass_context
+def tag_phase_2(
+    ctx,
+    month_offset,
+    artifact_env,
+    start_offset,
+    silence_no_files_to_process_exception,
+):
+    """Run processing and phase 2 tagging if output for phase 1 exists.
+
+    Example command:
+    ./phoenix-cli comments tag_phase_2 -1 production
+
+    MONTH_OFFSET: Number of months to offset by. E.g. 0 is current month, -1 is previous month.
+    ARTIFACT_ENV:
+        The artifact environment that will be used. Default "local"
+        Can use "production" which will pick the artifact env from the env var.
+        Or a valid storage URL like "s3://my-phoenix-bucket/"
+
+    Extra options will be added as parameters for all notebooks. E.g.
+    --SOME_URL='s3://other-bucket/` will be a parameter for all notebooks.
+    """
+    run_dt = run_datetime.create_run_datetime_now()
+    year_filter, month_filter = utils.get_year_month_for_offset(run_dt, month_offset)
+    art_url_reg = artifacts.registry.ArtifactURLRegistry(run_dt, artifact_env)
+    args_parameters = {
+        "OBJECT_TYPE": "facebook_comments",
+        "YEAR_FILTER": year_filter,
+        "MONTH_FILTER": month_filter,
+    }
+
+    extra_parameters = dict([item.strip("--").split("=") for item in ctx.args])
+    parameters = {
+        **utils.init_parameters(run_dt, art_url_reg),
+        **args_parameters,
+        **extra_parameters,
+    }
+
+    TAGGING_RUNS_URL_ASYNC_JOB_GROUP = art_url_reg.get_url(
+        "tagging_runs-async_job_group", parameters
+    )
+    try:
+        tentaclio.open(TAGGING_RUNS_URL_ASYNC_JOB_GROUP, mode="r")
+    except FileNotFoundError:
+        message = f"There is no file from phase 1: {TAGGING_RUNS_URL_ASYNC_JOB_GROUP}"
+        if not silence_no_files_to_process_exception:
+            raise RuntimeError(message)
+        click.echo(message)
+        return
+
+    tagging._run_tagging_notebooks(2, "facebook_comments", parameters, art_url_reg, start_offset)
