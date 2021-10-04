a37b13f0abd2aa6a41a33284bc130ff8dd4dd7cf
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-10-04 15:57:58 +0200

    WIP

--- phoenix/common/cli_modules/graphing.py
+++ phoenix/common/cli_modules/graphing.py
@@ -97,3 +97,71 @@ def topic(
         "tag/graphing/facebook_topic_graph.ipynb"
     ]
     tagging.tagging_run_notebooks(notebooks, parameters, art_url_reg, start_offset)
+
+
+@graphing.command(
+    "retweet",
+    context_settings=dict(
+        ignore_unknown_options=True,
+        allow_extra_args=True,
+    ),
+)
+@click.argument("month_offset", type=click.INT, default=0)
+@click.argument("artifact_env", default="local", envvar="ARTIFACT_ENV")
+@click.option(
+    "--start_offset",
+    default=0,
+    help=("Start notebook from offset."),
+)
+@click.option(
+    "--silence_no_files_to_process_exception",
+    default=False,
+    type=click.BOOL,
+    help=(
+        "Silence no files to process exception\n"
+        "This is for when the CLI is used in cron jobs so that unnecessary errors are created."
+    ),
+)
+@click.pass_context
+def retweet(
+    ctx,
+    month_offset,
+    artifact_env,
+    start_offset,
+    silence_no_files_to_process_exception,
+):
+    """Run processing for retweet graph.
+
+    Example command:
+    ./phoenix-cli graphing retweet -1 production
+
+    MONTH_OFFSET: Number of months to offset by. E.g. 0 is current month, -1 is previous month.
+    ARTIFACT_ENV:
+        The artifact environment that will be used. Default "local"
+        Can use "production" which will pick the artifact env from the env var.
+        Or a valid storage URL like "s3://my-phoenix-bucket/"
+
+    Extra options will be added as parameters for all notebooks. E.g.
+    --SOME_URL='s3://other-bucket/` will be a parameter for all notebooks.
+    """
+    run_dt = run_datetime.create_run_datetime_now()
+    year_filter, month_filter = utils.get_year_month_for_offset(run_dt, month_offset)
+    art_url_reg = artifacts.registry.ArtifactURLRegistry(run_dt, artifact_env)
+    args_parameters = {
+        "OBJECT_TYPE": "tweets",
+        "YEAR_FILTER": year_filter,
+        "MONTH_FILTER": month_filter,
+    }
+
+    extra_parameters = dict([item.strip("--").split("=") for item in ctx.args])
+    parameters = {
+        **utils.init_parameters(run_dt, art_url_reg),
+        **args_parameters,
+        **extra_parameters,
+    }
+
+    notebooks = [
+        "tag/data_pull/twitter_pull_retweet.ipynb",
+        "tag/graphing/twitter_retweet_graph.ipynb"
+    ]
+    tagging.tagging_run_notebooks(notebooks, parameters, art_url_reg, start_offset)
