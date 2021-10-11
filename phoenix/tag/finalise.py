99cf00b52b9740def8a5749b5e136c451a3324e8
Andrew Sutjahjo <andrewsutjahjo@gmail.com>
2021-10-11 19:05:03 +0200

    Add inheritence for topics by comments

--- phoenix/tag/finalise.py
+++ phoenix/tag/finalise.py
@@ -2,6 +2,10 @@
 
 This will join objects and topic data frames to respective data source dataframe.
 """
+from typing import List, Optional
+
+import pandas as pd
+
 
 LANGUAGE_SENTIMENT_COLUMNS = [
     "language_sentiment",
@@ -16,6 +20,20 @@ TOPICS_COLUMNS = [
     "topic",
     "matched_features",
     "has_topic",
+]
+
+COMMENT_INHERITED_COLUMNS = [
+    "topics",
+    "has_topics",
+    "is_economic_labour_tension",
+    "is_political_tension",
+    "is_service_related_tension",
+    "is_community_insecurity_tension",
+    "is_sectarian_tension",
+    "is_environmental_tension",
+    "is_geopolitics_tension",
+    "is_intercommunity_relations_tension",
+    "has_tension",
 ]
 
 
@@ -82,3 +100,38 @@ def join_topics_to_facebook_comments(topics, facebook_comments):
     topics_df = topics_df.set_index("object_id")
     result_df = topics_df.join(facebook_comments_df, how="right")
     return result_df.reset_index()
+
+
+def inherit_facebook_comment_topics_from_facebook_posts_topics_df(
+    posts_df: pd.DataFrame,
+    comments_df: pd.DataFrame,
+    extra_inherited_cols: Optional[List[str]] = None,
+) -> pd.DataFrame:
+    """Joins comments to their parent posts and inherits tensions and topic(s) from parents.
+
+    posts_df (pd.DataFrame): posts with columns to inherit.
+    comments_df (pd.DataFrame) base comments which inherit certain characteristics from parents.
+    extra_inherited_cols (Optional[List[str]]) extra columns to overwrite with posts information.
+    """
+    if extra_inherited_cols:
+        inherited_columns = extra_inherited_cols + COMMENT_INHERITED_COLUMNS
+    else:
+        inherited_columns = COMMENT_INHERITED_COLUMNS
+
+    for col in inherited_columns:
+        if col not in posts_df.columns:
+            raise Exception(f"Column {col} not found in posts dataframe.")
+    if "post_id" not in comments_df.columns:
+        raise Exception("Column 'post_id' not found in comments dataframe.")
+
+    posts_df = posts_df[["url_post_id"] + inherited_columns]
+    posts_df["url_post_id"] = posts_df["url_post_id"].astype(int)
+    posts_df = posts_df.groupby("url_post_id").last()
+
+    comments_df = comments_df.drop(inherited_columns, axis=1, errors="ignore")
+
+    comments_df = pd.merge(
+        comments_df, posts_df, left_on="post_id", right_on="url_post_id", how="left"
+    )
+
+    return comments_df
--- tests/unit/tag/test_finalise.py
+++ tests/unit/tag/test_finalise.py
@@ -111,3 +111,70 @@ def test_join_topics_to_facebook_comments():
             }
         ),
     )
+
+
+def inherit_facebook_comment_topics_from_posts():
+    input_comments_df = pd.DataFrame(
+        {
+            "id": ["1", "2"],
+            "post_id": [123, 456],
+            "topics": ["[None]", "[None]"],
+            "has_topics": [False, False],
+            "is_economic_labour_tension": [False, False],
+            "is_political_tension": [False, False],
+            "is_service_related_tension": [False, False],
+            "is_community_insecurity_tension": [False, False],
+            "is_sectarian_tension": [False, False],
+            "is_environmental_tension": [False, False],
+            "is_geopolitics_tension": [False, False],
+            "is_intercommunity_relations_tension": [False, False],
+            "has_tension": [False, False],
+            "comments_only_column": ["some_str", "another_str"],
+        }
+    )
+
+    input_facebook_posts_topics_df = pd.DataFrame(
+        {
+            "id": ["1", "2", "3"],
+            "url_post_id": ["123", "456", "456"],
+            "topic": ["a", "a", "b"],
+            "has_topic": [True, True, True],
+            "topics": ["['a']", "['a', 'b']", "['a', 'b']"],
+            "has_topics": [True, True, True],
+            "is_economic_labour_tension": [True, True, True],
+            "is_political_tension": [True, True, True],
+            "is_service_related_tension": [True, True, True],
+            "is_community_insecurity_tension": [True, True, True],
+            "is_sectarian_tension": [True, True, True],
+            "is_environmental_tension": [True, True, True],
+            "is_geopolitics_tension": [True, True, True],
+            "is_intercommunity_relations_tension": [True, True, True],
+            "has_tension": [True, True, True],
+        }
+    )
+
+    # Default does not inherit the `topic` or `has_topic` columns
+    expected_comments_df = pd.DataFrame(
+        {
+            "id": ["1", "2"],
+            "post_id": [123, 456],
+            "topics": ["a", "a"],
+            "has_topics": [True, True],
+            "is_economic_labour_tension": [True, True],
+            "is_political_tension": [True, True],
+            "is_service_related_tension": [True, True],
+            "is_community_insecurity_tension": [True, True],
+            "is_sectarian_tension": [True, True],
+            "is_environmental_tension": [True, True],
+            "is_geopolitics_tension": [True, True],
+            "is_intercommunity_relations_tension": [True, True],
+            "has_tension": [True, True],
+            "comments_only_column": ["some_str", "another_str"],
+        }
+    )
+
+    output_df = finalise.inherit_facebook_comment_topics_from_facebook_posts_topics_df(
+        input_facebook_posts_topics_df, input_comments_df
+    )
+
+    pd.testing.assert_frame_equal(output_df, expected_comments_df)
