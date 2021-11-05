944fa3d10657c37de0bcc380c4cd37ef809f6599
Andrew Sutjahjo <andrewsutjahjo@gmail.com>
2021-11-05 13:47:47 +0100

    Add wide_to_long df for pulling the labels out of label sheet

--- phoenix/tag/labeling/pull_label_sheet.py
+++ phoenix/tag/labeling/pull_label_sheet.py
@@ -16,3 +16,44 @@ def is_valid_labeling_sheet(df: pd.DataFrame) -> bool:
             return False
 
     return True
+
+
+def extract_features_to_label_mapping_objects(df: pd.DataFrame) -> pd.DataFrame:
+    """Extract features to class mappings."""
+    if not is_valid_labeling_sheet(df):
+        ValueError(
+            f"Dataframe does not have correct columns to be an object labeling sheet. {df.columns}"
+        )
+
+    feature_to_label_df = wide_to_long_labels_features(df)
+
+    return feature_to_label_df
+
+
+def wide_to_long_labels_features(df: pd.DataFrame) -> pd.DataFrame:
+    """Returns a long dataframe for labels and their features.
+
+    Note: this is pretty brittle, refactor soon
+    """
+    df_labels = pd.DataFrame(columns=["object_id", "class", "unprocessed_features"])
+
+    for i in range(1, 6):
+        COLS = df.filter(like=f"label_{i}").columns
+        COLS = COLS.append(df.filter(like="object_id").columns)
+        temp_df = df[COLS].copy()
+        temp_df = temp_df.rename(
+            {f"label_{i}": "class", f"label_{i}_features": "unprocessed_features"}, axis=1
+        )
+        # replace empty strings with NaN to be able to use dropna
+        temp_df[["class", "unprocessed_features"]] = temp_df[
+            ["class", "unprocessed_features"]
+        ].replace(r"^\s*$", np.nan, regex=True)
+        temp_df = temp_df.dropna(subset=["class", "unprocessed_features"], how="all")
+
+        df_labels = df_labels.append(temp_df)
+
+    df_labels["unprocessed_features"] = df_labels["unprocessed_features"].str.split(",")
+    df_labels = df_labels.explode("unprocessed_features").reset_index(drop=True)
+    df_labels["unprocessed_features"] = df_labels["unprocessed_features"].fillna("")
+
+    return df_labels
--- tests/unit/tag/labeling/test_pull_label_sheet.py
+++ tests/unit/tag/labeling/test_pull_label_sheet.py
@@ -13,3 +13,49 @@ def test_is_valid_labeling_sheet():
 def test_is_valid_labeling_sheet_false():
     incorrect_df = pd.DataFrame(columns=["some_col"])
     assert not pull_label_sheet.is_valid_labeling_sheet(incorrect_df)
+
+
+def test_wide_to_long_labels_features():
+    input_df = pd.DataFrame(
+        {
+            "object_id": ["id_1", "id_2", "id_3"],
+            "label_1": ["dog", "cat", "animal"],
+            "label_1_features": ["speaks woof,bark", "meow", "alive"],
+            "label_2": ["animal", "animal", None],
+            "label_2_features": ["speaks woof,bark", None, None],
+        },
+        columns=[
+            "object_id",
+            "label_1",
+            "label_1_features",
+            "label_2",
+            "label_2_features",
+            "label_3",
+            "label_3_features",
+            "label_4",
+            "label_4_features",
+            "label_5",
+            "label_5_features",
+        ],
+    )
+
+    expected_df = pd.DataFrame(
+        {
+            "object_id": ["id_1", "id_1", "id_2", "id_3", "id_1", "id_1", "id_2"],
+            "class": ["dog", "dog", "cat", "animal", "animal", "animal", "animal"],
+            "unprocessed_features": [
+                "speaks woof",
+                "bark",
+                "meow",
+                "alive",
+                "speaks woof",
+                "bark",
+                "",
+            ],
+        },
+        columns=["object_id", "class", "unprocessed_features"],
+    )
+
+    actual_df = pull_label_sheet.wide_to_long_labels_features(input_df)
+
+    pd.testing.assert_frame_equal(actual_df, expected_df, check_like=True)
