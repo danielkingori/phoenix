38f6999bdff0c751bec57e5a79f871ce460810c4
Andrew Sutjahjo <andrewsutjahjo@gmail.com>
2021-11-08 16:22:06 +0100

    Process features from human-annotated file

--- phoenix/tag/labeling/pull_label_sheet.py
+++ phoenix/tag/labeling/pull_label_sheet.py
@@ -1,7 +1,10 @@
 """Pull the labeling sheet with human-annotated labels."""
+from typing import Tuple
 
+import numpy as np
 import pandas as pd
 
+from phoenix.tag import language, text_features_analyser
 from phoenix.tag.labeling.generate_label_sheet import EXPECTED_COLUMNS_OBJECT_LABELING_SHEET
 
 
@@ -18,16 +21,60 @@ def is_valid_labeling_sheet(df: pd.DataFrame) -> bool:
     return True
 
 
-def extract_features_to_label_mapping_objects(df: pd.DataFrame) -> pd.DataFrame:
-    """Extract features to class mappings."""
+def extract_features_to_label_mapping_objects(
+    df: pd.DataFrame,
+) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """Extract features to class mappings.
+
+    Args:
+        df (pd.DataFrame): filled in manually labeled sheet as defined by
+            EXPECTED_COLUMNS_LABELING_SHEET
+
+    Returns:
+        df (pd.DataFrame): Feature to label mapping dataframe
+        df (pd.DataFrame): objects with labels with no features dataframe
+
+    Runs the language detection from tag.language on the "text" column, and uses that language
+    to run the feature processing pipeline without splitting text into ngrams.
+    """
     if not is_valid_labeling_sheet(df):
         ValueError(
             f"Dataframe does not have correct columns to be an object labeling sheet. {df.columns}"
         )
 
     feature_to_label_df = wide_to_long_labels_features(df)
+    feature_to_label_df = feature_to_label_df.merge(
+        df[["object_id", "text"]], how="left", on="object_id"
+    )
+    feature_to_label_df[["language", "language_confidence"]] = language.execute(
+        feature_to_label_df["text"]
+    )
 
-    return feature_to_label_df
+    tfa = text_features_analyser.create(use_ngrams=False)
+    feature_to_label_df["processed_features"] = tfa.features(
+        feature_to_label_df, "unprocessed_features"
+    )
+
+    # This defensive cast through mapping each list of strings to a string should never be
+    # needed over just a str(str_list), but just in case, we've gone defensive.
+    feature_to_label_df["processed_features"] = [
+        " ".join(map(str, str_list)) for str_list in feature_to_label_df["processed_features"]
+    ]
+
+    feature_to_label_df["use_processed_features"] = True
+    feature_to_label_df["status"] = "active"
+
+    feature_to_label_df["unprocessed_features"] = feature_to_label_df[
+        "unprocessed_features"
+    ].replace(r"^\s*$", np.nan, regex=True)
+    label_with_no_feature_df = feature_to_label_df[
+        feature_to_label_df["unprocessed_features"].isna()
+    ].fillna("")
+    feature_to_label_df = feature_to_label_df.drop("text", axis=1)
+    feature_to_label_df = feature_to_label_df.dropna(subset=["unprocessed_features"])
+    feature_to_label_df = feature_to_label_df.fillna("")
+
+    return feature_to_label_df, label_with_no_feature_df
 
 
 def wide_to_long_labels_features(df: pd.DataFrame) -> pd.DataFrame:
--- tests/unit/tag/labeling/test_pull_label_sheet.py
+++ tests/unit/tag/labeling/test_pull_label_sheet.py
@@ -1,4 +1,5 @@
 """Test pull_label_sheet."""
+import mock
 import pandas as pd
 
 from phoenix.tag.labeling import pull_label_sheet
@@ -59,3 +60,119 @@ def test_wide_to_long_labels_features():
     actual_df = pull_label_sheet.wide_to_long_labels_features(input_df)
 
     pd.testing.assert_frame_equal(actual_df, expected_df, check_like=True)
+
+
+@mock.patch("phoenix.tag.labeling.pull_label_sheet.language.execute")
+def test_extract_features_to_label_mapping(mock_execute):
+    mock_execute.return_value = pd.DataFrame(data=[("en", 99.5)] * 7)
+    input_df = pd.DataFrame(
+        {
+            "object_id": ["id_1", "id_2", "id_3"],
+            "text": ["this thing speaks woof and bark", "this goes meow", "this is alive"],
+            "label_1": ["dog", "cat", "animal"],
+            "label_1_features": ["speaks woof,bark", "meow", "alive"],
+            "label_2": ["animal", "animal", None],
+            "label_2_features": ["speaks woof,bark", None, None],
+        },
+        columns=EXPECTED_COLUMNS_OBJECT_LABELING_SHEET,
+    )
+
+    expected_df = pd.DataFrame(
+        {
+            "object_id": ["id_1", "id_1", "id_1", "id_1", "id_2", "id_3"],
+            "class": ["dog", "dog", "animal", "animal", "cat", "animal"],
+            "unprocessed_features": [
+                "speaks woof",
+                "bark",
+                "speaks woof",
+                "bark",
+                "meow",
+                "alive",
+            ],
+            "language": ["en"] * 6,
+            "language_confidence": [99.5] * 6,
+            "processed_features": ["speak woof", "bark", "speak woof", "bark", "meow", "aliv"],
+            "use_processed_features": [True] * 6,
+            "status": ["active"] * 6,
+        },
+        columns=[
+            "object_id",
+            "class",
+            "unprocessed_features",
+            "language",
+            "language_confidence",
+            "processed_features",
+            "use_processed_features",
+            "status",
+        ],
+    )
+
+    actual_df, _ = pull_label_sheet.extract_features_to_label_mapping_objects(input_df)
+
+    pd.testing.assert_frame_equal(
+        actual_df.sort_values(by=["object_id", "class"]).reset_index(drop=True),
+        expected_df.sort_values(by=["object_id", "class"]).reset_index(drop=True),
+    )
+
+
+@mock.patch("phoenix.tag.labeling.pull_label_sheet.language.execute")
+def test_extract_features_to_label_mapping_no_features(mock_execute):
+    mock_execute.return_value = pd.DataFrame(data=[("en", 99.5)] * 7)
+    input_df = pd.DataFrame(
+        {
+            "object_id": ["note to user about the object_id", "id_1", "id_2", "id_3"],
+            "text": [
+                "note to user about the text",
+                "this thing speaks woof and bark",
+                "this goes meow",
+                "this is alive",
+            ],
+            "label_1": ["note to user about label_1", "dog", "cat", "animal"],
+            "label_1_features": [
+                "note to user about label_1_features",
+                "speaks woof,bark",
+                "meow",
+                "alive",
+            ],
+            "label_2": ["note to user about label_2", "animal", "animal", None],
+            "label_2_features": [
+                "note to user about label_2_features",
+                "speaks woof,bark",
+                None,
+                None,
+            ],
+        },
+        columns=EXPECTED_COLUMNS_OBJECT_LABELING_SHEET,
+    )
+
+    expected_df = pd.DataFrame(
+        {
+            "object_id": ["id_2"],
+            "class": ["animal"],
+            "unprocessed_features": [""],
+            "text": ["this goes meow"],
+            "language": ["en"],
+            "language_confidence": [99.5],
+            "processed_features": [""],
+            "use_processed_features": [True],
+            "status": ["active"],
+        },
+        columns=[
+            "object_id",
+            "class",
+            "unprocessed_features",
+            "text",
+            "language",
+            "language_confidence",
+            "processed_features",
+            "use_processed_features",
+            "status",
+        ],
+    )
+
+    _, actual_df = pull_label_sheet.extract_features_to_label_mapping_objects(input_df)
+
+    pd.testing.assert_frame_equal(
+        actual_df.sort_values(by=["object_id", "class"]).reset_index(drop=True),
+        expected_df.sort_values(by=["object_id", "class"]).reset_index(drop=True),
+    )
