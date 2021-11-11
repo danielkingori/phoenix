c5788c3b5720cc5b144f0f38276556e8f9fd34fc
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-11-11 15:05:09 +0100

    core/multitenant_v3 nested dataclasses_utils

--- phoenix/common/dataclasses_utils.py
+++ phoenix/common/dataclasses_utils.py
@@ -4,7 +4,7 @@ import json
 
 
 def are_equal(dc_1, dc_2):
-    """Are dataclasses equal."""
+    """Are dataclasses equal for nested dataclasses."""
     dc_1_dict = dataclasses.asdict(dc_1)
     dc_2_dict = dataclasses.asdict(dc_2)
     return json.dumps(dc_1_dict, sort_keys=True) == json.dumps(dc_2_dict, sort_keys=True)
