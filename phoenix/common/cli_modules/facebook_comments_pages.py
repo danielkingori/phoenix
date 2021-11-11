53199c9e177e1568950e5d371db43419b90b88cc
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-11-11 16:30:19 +0100

    core/multitenant_v3 cli_modules/facebook_comments_pages.py

--- phoenix/common/cli_modules/facebook_comments_pages.py
+++ phoenix/common/cli_modules/facebook_comments_pages.py
@@ -1,7 +1,7 @@
 """Facebook Comments Pages CLI commands."""
 import click
 
-from phoenix.common import artifacts, run_datetime
+from phoenix.common import run_params
 from phoenix.common.cli_modules import main_group
 
 
@@ -11,23 +11,30 @@ def facebook_comments_pages():
 
 
 @facebook_comments_pages.command("aws_copy_command")
+@click.argument("artifact_env")
+@click.argument("tenant_id")
 @click.argument("input_directory")
 @click.argument("year_filter")
 @click.argument("month_filter")
-@click.argument("artifact_env", default="production")
 def cp_facebook_comments_pages(
+    artifact_env,
+    tenant_id,
     input_directory,
     year_filter,
     month_filter,
-    artifact_env,
 ):
     """Get aws command to copy the downloaded file to s3.
 
     This is a not yet complete but a quick sketch to get something together.
 
     Example comment:
-    ./phoenix-cli cp_facebook_comments_pages 2021 8 production
+    ./phoenix-cli cp_facebook_comments_pages production tenant 2021 8
 
+    ARTIFACT_ENV:
+        The artifact environment that will be used.
+        Can use "production" which will pick the artifact env from the env var.
+        Or a valid storage URL like "s3://my-phoenix-bucket/"
+    TENANT_ID: The id of the tenant to run phoenix for.
     INPUT_DIRECTORY:
         String of the input directory argument for the command.
         A simple solution to this is "." and then `cd` to the correct directory
@@ -37,15 +44,13 @@ def cp_facebook_comments_pages(
     MONTH_FILTER:
         The month as a number that the posts that have been downloaded have been created.
         (For August) eg. 8
-    ARTIFACT_ENV:
-        The artifact environment that will be used. Default "production"
-        Can use a valid storage URL like "s3://my-phoenix-bucket/"
     """
-    run_dt = run_datetime.create_run_datetime_now()
-    art_url_reg = artifacts.registry.ArtifactURLRegistry(run_dt, artifact_env)
+    cur_run_params = run_params.general.create(artifact_env, tenant_id)
     url_config = {
         "YEAR_FILTER": year_filter,
         "MONTH_FILTER": month_filter,
     }
-    base_to_parse = art_url_reg.get_url("base-facebook_comments_pages_to_parse", url_config)
+    base_to_parse = cur_run_params.art_url_reg.get_url(
+        "base-facebook_comments_pages_to_parse", url_config
+    )
     click.echo(f"Command to run: aws s3 cp --recursive {input_directory} {base_to_parse}")
