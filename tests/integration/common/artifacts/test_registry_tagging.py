e8df5010d81634b240a55045a53152b95880bbeb
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-11-12 07:22:55 +0100

    core/multitenant_v3 default_url_prefix with tenant_id
    This changes a few of the integration test to include
    the tenant_id functionality.
    
    There are many more integration tests for the mappers that have
    not includes tests for the tenant_id functionality.
    They however pass use to using `.endswith(` meaning that
    the resultant string still ends with the same value.
    
    See: tests/integration/common/artifacts/test_registry_base.py
    for an example of this.

--- tests/integration/common/artifacts/test_registry_tagging.py
+++ tests/integration/common/artifacts/test_registry_tagging.py
@@ -12,52 +12,60 @@ from tests.integration.common.artifacts import conftest
 @freeze_time("2000-01-01 T01:01:01.000001Z")
 @mock.patch.dict(os.environ, {registry_environment.PRODUCTION_ENV_VAR_KEY: "s3://data-lake/"})
 @pytest.mark.parametrize(
-    "artifact_key, url_config, environment_key, expected_url",
+    "artifact_key, url_config, environment_key, expected_url, tenant_id",
     [
         (
             "tagging_runs-facebook_posts_input",
             {"YEAR_FILTER": 2021, "MONTH_FILTER": 1},
             "local",
-            "base/grouped_by_year_month/facebook_posts/year_filter=2021/month_filter=1/",
+            (
+                "/test_tenant/"
+                "base/grouped_by_year_month/facebook_posts/year_filter=2021/month_filter=1/"
+            ),
+            "test_tenant",
         ),
         (
             "tagging_runs-facebook_posts_pulled",
             {"YEAR_FILTER": 2021, "MONTH_FILTER": 1},
             "local",
             (
-                "tagging_runs/year_filter=2021/month_filter=1/"
+                "test_tenant/tagging_runs/year_filter=2021/month_filter=1/"
                 "facebook_posts/facebook_posts_pulled.parquet"
             ),
+            "test_tenant",
         ),
         (
             "tagging_runs-facebook_posts_for_tagging",
             {"YEAR_FILTER": 2021, "MONTH_FILTER": 1},
             "local",
             (
-                "tagging_runs/year_filter=2021/month_filter=1/"
+                "test_tenant/tagging_runs/year_filter=2021/month_filter=1/"
                 "facebook_posts/for_tagging/facebook_posts_for_tagging.parquet"
             ),
+            "test_tenant",
         ),
         (
             "tagging_runs-pipeline_base",
             {"YEAR_FILTER": 2021, "MONTH_FILTER": 1, "OBJECT_TYPE": "facebook_posts"},
             "local",
-            "tagging_runs/year_filter=2021/month_filter=1/facebook_posts/",
+            "test_tenant/tagging_runs/year_filter=2021/month_filter=1/facebook_posts/",
+            "test_tenant",
         ),
         (
             "tagging_runs-output_notebook_base",
             {"YEAR_FILTER": 2021, "MONTH_FILTER": 1, "OBJECT_TYPE": "facebook_posts"},
             "production",
             (
-                "s3://data-lake/"
+                "s3://data-lake/test_tenant/"
                 "tagging_runs/year_filter=2021/month_filter=1/facebook_posts/"
                 "output_notebooks/20000101T010101.000001Z/"
             ),
+            "test_tenant",
         ),
     ],
 )
-def test_tagging_runs_urls(artifact_key, url_config, environment_key, expected_url):
+def test_tagging_runs_urls(artifact_key, url_config, environment_key, expected_url, tenant_id):
     """Test tagging runs urls."""
-    art_url_reg = conftest.create_test_art_url_reg(environment_key)
+    art_url_reg = conftest.create_test_art_url_reg(environment_key, tenant_id)
     result_url = art_url_reg.get_url(artifact_key, url_config)
     assert result_url.endswith(expected_url)
