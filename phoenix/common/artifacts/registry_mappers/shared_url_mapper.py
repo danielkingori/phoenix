0b1491f5dfc018706441594b11c65e83e24005ea
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-11-12 07:05:00 +0100

    core/multitenant_v3 tenant_config ArtifactURLMapper

--- phoenix/common/artifacts/registry_mappers/shared_url_mapper.py
+++ phoenix/common/artifacts/registry_mappers/shared_url_mapper.py
@@ -3,13 +3,15 @@ from typing import Any, Dict
 
 from phoenix.common.artifacts import registry_environment as reg_env
 from phoenix.common.artifacts.registry_mappers import artifact_keys
+from phoenix.common.config import tenant
 
 
 def dashboard_url_mapper(
     format_str: str,
     artifact_key: artifact_keys.ArtifactKey,
     url_config: Dict[str, Any],
-    environment_key: reg_env.Environments = reg_env.DEFAULT_ENVIRONMENT_KEY,
+    environment_key: reg_env.Environments,
+    tenant_config: tenant.TenantConfig,
 ):
     """Dashboard url mapper.
 
