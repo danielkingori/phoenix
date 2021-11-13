47a6f83d613aa1e80a4fdf2fe2459ec7edf00ac9
dbcerigo <dbcerigo@gmail.com>
2021-11-13 12:56:06 +0000

    Refactor Google Sheets interface
    This refactors the interface in the following ways:
    - Enables usage of patched `open` to pass `folder_id`
    - Separates creating Sheets and persisting to them
    - Makes `persist` and `get` DRY

--- phoenix/common/artifacts/google_sheets.py
+++ phoenix/common/artifacts/google_sheets.py
@@ -1,4 +1,6 @@
 """Getting and persisting dataframes from and to Google Sheets."""
+from typing import List
+
 import os
 
 import gspread
@@ -11,21 +13,40 @@ def get_client():
     return gspread_client
 
 
+def create_sheet(gspread_client, folder_id: str, sheet_name: str, worksheet_names: List[str]):
+    """Create a sheet and worksheets within sheet.
+
+    To further add or edit worksheets to a sheet already created, one should do so manually either
+    via browser or use gspread directly.
+    """
+    sheet = gspread_client.create(sheet_name, folder_id=folder_id)
+    for worksheet_name in worksheet_names:
+        sheet.add_worksheet(title=worksheet_name, rows=0, cols=0)
+
+
+def _get_worksheet(
+    gspread_client, folder_id: str, sheet_name: str, worksheet_name: str
+) -> gspread.Worksheet:
+    """Get worksheet object for given folder ID, sheet name, and worksheet name."""
+    sheet = gspread_client.open(sheet_name, folder_id)
+    return sheet.worksheet(worksheet_name)
+
+
 def persist(
     gspread_client, folder_id: str, sheet_name: str, worksheet_name: str, df: pd.DataFrame
 ):
     """Persist dataframe to worksheet of sheet.
 
-    WARNING: Sheet names must be unique within all Sheets accessible to Google account (likely a
-    Service Account), otherwise it will pick the "first" sheet with given name it finds.
+    WARNING: Sheet names must be unique within folder for folder ID given otherwise it will pick
+    the "first" sheet with given name it finds in the folder. Note that this is a patched version
+    of `gspread` allowing to use `folder_id` in `open` which massively reduces sheet name clash
+    issues.
     """
-    sheet = gspread_client.create(sheet_name, folder_id=folder_id)
-    worksheet = sheet.add_worksheet(title=worksheet_name, rows=0, cols=0)
+    worksheet = _get_worksheet(gspread_client, folder_id, sheet_name, worksheet_name)
     worksheet.update([df.columns.values.tolist()] + df.values.tolist())
 
 
 def get(gspread_client, folder_id: str, sheet_name: str, worksheet_name: str) -> pd.DataFrame:
     """Get dataframe of worksheet of sheet in folder."""
-    sheet = gspread_client.open(sheet_name)
-    worksheet = sheet.worksheet(worksheet_name)
+    worksheet = _get_worksheet(gspread_client, folder_id, sheet_name, worksheet_name)
     return pd.DataFrame(worksheet.get_all_records())
