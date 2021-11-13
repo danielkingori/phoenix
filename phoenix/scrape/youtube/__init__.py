8e1298fe212877abc2263b68aef078e6c473b817
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-11-13 12:05:34 +0100

    scrape/youtube_v1 docstring for init

--- phoenix/scrape/youtube/__init__.py
+++ phoenix/scrape/youtube/__init__.py
@@ -1,2 +1,10 @@
-"""Init Youtube."""
+"""Init Youtube.
+
+Important docs:
+    SDK docs: https://googleapis.github.io/google-api-python-client/docs/dyn/youtube_v3.html
+    Samples: https://github.com/youtube/api-samples
+    Docs and guides for all apis:
+    https://github.com/googleapis/google-api-python-client/tree/main/docs
+    API Reference: https://developers.google.com/youtube/v3/docs
+"""
 from phoenix.scrape.youtube import utils  # noqa: F401
