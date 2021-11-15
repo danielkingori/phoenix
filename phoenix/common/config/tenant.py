c3cc06c16741f268748381d18447a6a1da69fb6a
dbcerigo <dbcerigo@gmail.com>
2021-11-15 14:54:56 +0000

    Implement tenant.get_configs()

--- phoenix/common/config/tenant.py
+++ phoenix/common/config/tenant.py
@@ -1,7 +1,12 @@
 """Tenant configuration."""
-from typing import Optional
+from typing import List, Optional
 
 import dataclasses
+
+import tentaclio
+import yaml
+
+from phoenix.common import artifacts
 
 
 @dataclasses.dataclass
@@ -11,3 +16,16 @@ class TenantConfig:
     id: str
     google_drive_folder_id: Optional[str] = None
     crowdtangle_scrape_list_id: Optional[str] = None
+
+
+def get_configs(
+    file_name: str = "tenants.yaml", file_dir: Optional[str] = None
+) -> List[TenantConfig]:
+    """Get list of all tenant configs."""
+    if file_dir is None:
+        file_dir = artifacts.urls.get_static_config()
+    yaml_path = file_dir + file_name
+    with tentaclio.open(yaml_path, "r") as fh:
+        processed_yaml = yaml.safe_load(fh)
+    tenant_configs = [TenantConfig(**d) for d in processed_yaml["tenants"]]
+    return tenant_configs
--- tests/unit/common/config/test_tenant.py
+++ tests/unit/common/config/test_tenant.py
@@ -8,3 +8,27 @@ def test_tenant_config():
     id_str = "id"
     tenant_config = tenant.TenantConfig(id_str)
     assert tenant_config.id == id_str
+
+
+def test_get_tenant_configs():
+    """Test getting tenant configs.
+
+    Note this uses the template yaml, which is good because it ensures the template that users see
+    matches the interface and functionality.
+    """
+    expected_tenant_configs = [
+        tenant.TenantConfig(
+            "tenant_id_1",
+            google_drive_folder_id="folder_id_1",
+            crowdtangle_scrape_list_id="list_id_1",
+        ),
+        tenant.TenantConfig("tenant_id_2", crowdtangle_scrape_list_id="list_id_2"),
+        tenant.TenantConfig(
+            "tenant_id_3",
+            google_drive_folder_id="folder_id_3",
+        ),
+    ]
+
+    out_tenant_configs = tenant.get_configs(file_name="tenants_template.yaml")
+    for expected, out in zip(expected_tenant_configs, out_tenant_configs):
+        assert expected == out
