e156abb9a338ee5b234b17439f293749522072c7
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-11-15 11:16:58 +0100

    scrape/youtube_v1 mocked integration tests

--- tests/integration/scrape/youtube/conftest.py
+++ tests/integration/scrape/youtube/conftest.py
@@ -5,7 +5,31 @@ from phoenix.common import utils
 
 
 @pytest.fixture
-def youtube_channel_config_url():
+def mock_data_folder():
+    """Mock data folder path."""
+    return str(utils.relative_path("./data/", __file__)) + "/"
+
+
+@pytest.fixture
+def youtube_channel_config_url(mock_data_folder):
     """URL for the test youtube channel config."""
-    config_path = utils.relative_path("./channels_config.csv", __file__)
-    return f"file:///{config_path}"
+    return f"file:///{mock_data_folder}channels_config.csv"
+
+
+def read_youtube_response(path):
+    """Read the youtube response."""
+    with open(path) as f:
+        result = f.read()
+    return result
+
+
+@pytest.fixture
+def youtube_channel_page_final(mock_data_folder):
+    """Youtube Channel final page (no next page)."""
+    return read_youtube_response(mock_data_folder + "channels_page_final.json")
+
+
+@pytest.fixture
+def youtube_channel_page_with_next(mock_data_folder):
+    """Youtube Channel page with next page."""
+    return read_youtube_response(mock_data_folder + "channels_page_with_next.json")
