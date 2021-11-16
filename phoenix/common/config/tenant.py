1fd18112670164f32f50de0842ef95bcb906f070
dbcerigo <dbcerigo@gmail.com>
2021-11-16 15:15:58 +0000

    Refactor to use `config_url` param
    The intention here is that one would make calls to get the config for a
    tenant like:
    ```python
    
    from phoenix.common.artifacts import registry_environment
    from phoenix.common.config import tenant
    
    tenant_config = tenant.get_config(tenant_id, registry_environment.config_url_prefix(environment_key))
    ```

--- phoenix/common/config/tenant.py
+++ phoenix/common/config/tenant.py
@@ -6,8 +6,6 @@ import dataclasses
 import tentaclio
 import yaml
 
-from phoenix.common import artifacts
-
 
 @dataclasses.dataclass
 class TenantConfig:
@@ -18,24 +16,18 @@ class TenantConfig:
     crowdtangle_scrape_list_id: Optional[str] = None
 
 
-def get_configs(
-    file_name: str = "tenants.yaml", file_dir: Optional[str] = None
-) -> List[TenantConfig]:
+def get_configs(config_url: str) -> List[TenantConfig]:
     """Get list of all tenant configs."""
-    if file_dir is None:
-        file_dir = artifacts.urls.get_static_config()
-    yaml_path = file_dir + file_name
-    with tentaclio.open(yaml_path, "r") as fh:
+    file_url = config_url + "tenants.yaml"
+    with tentaclio.open(file_url, "r") as fh:
         processed_yaml = yaml.safe_load(fh)
     tenant_configs = [TenantConfig(**d) for d in processed_yaml["tenants"]]
     return tenant_configs
 
 
-def get_config(
-    tenant_id: str, file_name: str = "tenants.yaml", file_dir: Optional[str] = None
-) -> TenantConfig:
+def get_config(tenant_id: str, config_url: str) -> TenantConfig:
     """Get config for tenant ID."""
-    tenant_configs = get_configs(file_name, file_dir)
+    tenant_configs = get_configs(config_url)
     tenant_config = [config for config in tenant_configs if config.id == tenant_id]
     if len(tenant_config) > 1:
         raise ValueError(f"Multiples tenants found for [tenant_id={tenant_id}].")
--- tests/unit/common/config/test_tenant.py
+++ tests/unit/common/config/test_tenant.py
@@ -1,6 +1,7 @@
 """Test Tenant configuration."""
 import pytest
 
+from phoenix.common import artifacts
 from phoenix.common.config import tenant
 
 
@@ -11,7 +12,12 @@ def test_tenant_config():
     assert tenant_config.id == id_str
 
 
-def test_get_tenant_configs():
+@pytest.fixture
+def tenants_template_config_url() -> str:
+    return artifacts.urls.get_static_config()
+
+
+def test_get_tenant_configs(tenants_template_config_url):
     """Test getting tenant configs.
 
     Note this uses the template yaml, which is good because it ensures the template that users see
@@ -30,18 +36,18 @@ def test_get_tenant_configs():
         ),
     ]
 
-    out_tenant_configs = tenant.get_configs(file_name="tenants_template.yaml")
+    out_tenant_configs = tenant.get_configs(tenants_template_config_url)
     for expected, out in zip(expected_tenant_configs, out_tenant_configs):
         assert expected == out
 
 
-def test_get_tenant_config():
+def test_get_tenant_config(tenants_template_config_url):
     """Test getting tenant config for tenant ID."""
     expected_config = tenant.TenantConfig(
         "tenant_id_3",
         google_drive_folder_id="folder_id_3",
     )
-    assert expected_config == tenant.get_config("tenant_id_3", file_name="tenants_template.yaml")
+    assert expected_config == tenant.get_config("tenant_id_3", tenants_template_config_url)
 
 
 def test_get_tenant_config_raises_for_tenant_not_found(tmp_path):
@@ -51,10 +57,10 @@ def test_get_tenant_config_raises_for_tenant_not_found(tmp_path):
         -
             id: tenant_id_1
     """
-    path = tmp_path / "test.yaml"
+    path = tmp_path / "tenants.yaml"
     path.write_text(raw_yaml)
     with pytest.raises(ValueError):
-        tenant.get_config("tenant_id_2", file_name=str(path), file_dir="")
+        tenant.get_config("tenant_id_2", str(tmp_path) + "/")
 
 
 def test_get_tenant_config_raises_for_multiple_tenants_found(tmp_path):
@@ -66,7 +72,7 @@ def test_get_tenant_config_raises_for_multiple_tenants_found(tmp_path):
         -
             id: tenant_id_1
     """
-    path = tmp_path / "test.yaml"
+    path = tmp_path / "tenants.yaml"
     path.write_text(raw_yaml)
     with pytest.raises(ValueError):
-        tenant.get_config("tenant_id_1", file_name=str(path), file_dir="")
+        tenant.get_config("tenant_id_1", str(tmp_path) + "/")
