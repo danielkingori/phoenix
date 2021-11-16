f8abffb35b9cdae3a1ac61d10b2d108e7f8a1788
dbcerigo <dbcerigo@gmail.com>
2021-11-16 12:04:07 +0000

    fixup! Add env vars .sh template file

--- .env_vars_template.sh
+++ .env_vars_template.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# This file can be used by:
+# - making a copy to `.env` so that it (and its secrets) are out of version control
+# - filling in the variables that are needed
+# - running `source .env` to export all the variables into the current session
+# - starting up a docker or a jupyter server
+
+
 # CrowdTangle auth token
 export CROWDTANGLE_API_TOKEN=""
-export CT_RATE_LIMIT_CALLS=30
+export CT_RATE_LIMIT_CALLS=6
 # This needs to be refactored in to tenants.yaml
 export CROWDTANGLE_SCRAPE_LIST_ID=""
 
@@ -25,11 +32,8 @@ export GOOGLE_APPLICATION_CREDENTIALS=""
 
 # Production storage bucket URLs
 export PRODUCTION_ARTIFACTS_URL_PREFIX=""
-export PRODUCTION_DASHBOARD_URL_PREFIX=""
+# Optional:
+# export PRODUCTION_DASHBOARD_URL_PREFIX=""
 
 # Dask cluster
 export DASK_CLUSTER_IP=tcp://127.0.0.1:8786
-
-# This file can be used via `source path_to_env_file.sh`
-# to add all env vars pre starting a docker container
-# or starting a jupyter server
