8f5a940f1409fdae16f4791704598e8ec92fb5eb
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-11-16 10:52:32 +0100

    scrape/youtube_v1 cur_chanels_config -> channels_config

--- phoenix/scrape/youtube/channels.py
+++ phoenix/scrape/youtube/channels.py
@@ -20,7 +20,7 @@ DEFAULT_PARTS_TO_REQUEST = [
 
 
 def get_channels(
-    cur_chanels_config: pd.DataFrame,
+    channels_config: pd.DataFrame,
     parts_list: Optional[List[str]] = None,
     client: Optional[discovery.Resource] = None,
 ) -> List[Any]:
@@ -30,7 +30,7 @@ def get_channels(
     https://developers.google.com/resources/api-libraries/documentation/youtube/v3/python/latest/youtube_v3.channels.html
 
     Arguments:
-        cur_chanels_config (DataFrame): A channels config dataframe see: ./channels_config.py
+        channels_config (DataFrame): A channels config dataframe see: ./channels_config.py
         parts_list: An optional list of parts that should be requested. Default is None.
             If None then DEFAULT_PARTS_TO_REQUEST is used.
             See:
@@ -42,14 +42,14 @@ def get_channels(
     """
     channels = utils.get_resource_client("channels", client)
     part_str = _get_part_str(parts_list)
-    channel_ids_str = _get_channel_ids_str(cur_chanels_config)
+    channel_ids_str = _get_channel_ids_str(channels_config)
     request = channels.list(part=part_str, id=channel_ids_str)
     return lists.paginate_list_resource(channels, request)
 
 
-def _get_channel_ids_str(cur_chanels_config: pd.DataFrame):
+def _get_channel_ids_str(channels_config: pd.DataFrame):
     """Get the channel_ids_str."""
-    return ",".join(cur_chanels_config["channel_id"].values)
+    return ",".join(channels_config["channel_id"].values)
 
 
 def _get_part_str(
