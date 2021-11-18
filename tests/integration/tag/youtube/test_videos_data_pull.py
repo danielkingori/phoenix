3e8a783fb2823419bd1aaf119bcb349182c2f93f
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-11-18 12:33:59 +0100

    tag/youtube_videos integration test and expected pandas

--- tests/integration/tag/youtube/test_videos_data_pull.py
+++ tests/integration/tag/youtube/test_videos_data_pull.py
@@ -1,5 +1,8 @@
 """Test Youtube videos data puller."""
+import datetime
 import pytest
+
+import pandas as pd
 
 from phoenix.common import utils
 from phoenix.tag.data_pull import youtube_videos
@@ -21,4 +24,36 @@ def test_videos_data_pull(youtube_videos_source_folder_url):
     - filter for month and date.
     """
     result = youtube_videos.execute(youtube_videos_source_folder_url, 2000, 1)
-    assert result.shape[0] != 0
+    expected = pd.DataFrame([
+        {
+            "id": "video_1-id",
+            "created_at": datetime.datetime(2000, 1, 1, 0, 0, 0),
+            "channel_id": "video_1-channel_id",
+            "title": "video_1-title",
+            "description": "video_1-description",
+            "text": "video_1-title video_1-description",
+            "channel_title": "video_1-channel_title",
+            "video_url": "https://www.youtube.com/watch?v=video_1=id",
+        },
+        {
+            "id": "video_2-id",
+            "created_at": datetime.datetime(2000, 1, 2, 2, 26, 31),
+            "channel_id": "video_2-channel_id",
+            "title": "video_2-title_updated",
+            "description": "video_2-description_updated",
+            "text": "video_2-title-updated video_2-description_updated",
+            "channel_title": "video_2-channel_title_updated",
+            "video_url": "https://www.youtube.com/watch?v=video_2=id",
+        },
+        {
+            "id": "video_3-id",
+            "created_at": datetime.datetime(2000, 1, 31, 23, 59, 59),
+            "channel_id": "video_3-channel_id",
+            "title": "video_3-title_updated",
+            "description": "video_3-description_updated",
+            "text": "video_3-title-updated video_3-description_updated",
+            "channel_title": "video_3-channel_title_updated",
+            "video_url": "https://www.youtube.com/watch?v=video_3=id",
+        },
+    ])
+    pd.testing.assert_frame_equal(result, expected)
