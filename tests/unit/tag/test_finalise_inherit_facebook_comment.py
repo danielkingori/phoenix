d554fd18d1f4df2bfc78a9e4dca1658c75d7803d
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-12-07 10:37:07 +0100

    tag/topic_to_class_finalise add of the non_topic

--- tests/unit/tag/test_finalise_inherit_facebook_comment.py
+++ tests/unit/tag/test_finalise_inherit_facebook_comment.py
@@ -7,45 +7,47 @@ from phoenix.tag import finalise
 
 @pytest.fixture
 def input_comments_df():
+    row_count = 3
     return pd.DataFrame(
         {
-            "id": ["1", "2"],
-            "post_id": [123, 456],
-            "topics": [[], []],
-            "has_topics": [False, False],
-            "is_economic_labour_tension": [False, False],
-            "is_political_tension": [False, False],
-            "is_service_related_tension": [False, False],
-            "is_community_insecurity_tension": [False, False],
-            "is_sectarian_tension": [False, False],
-            "is_environmental_tension": [False, False],
-            "is_geopolitics_tension": [False, False],
-            "is_intercommunity_relations_tension": [False, False],
-            "has_tension": [False, False],
-            "comments_only_column": ["some_str", "another_str"],
+            "id": ["1", "2", "3"],
+            "post_id": [123, 456, 789],
+            "topics": [[], [], ["c_t"]],
+            "has_topics": [False, False, True],
+            "is_economic_labour_tension": [False] * row_count,
+            "is_political_tension": [False] * row_count,
+            "is_service_related_tension": [False] * row_count,
+            "is_community_insecurity_tension": [False] * row_count,
+            "is_sectarian_tension": [False] * row_count,
+            "is_environmental_tension": [False] * row_count,
+            "is_geopolitics_tension": [False] * row_count,
+            "is_intercommunity_relations_tension": [False] * row_count,
+            "has_tension": [False] * row_count,
+            "comments_only_column": ["some_str"] * row_count,
         }
     )
 
 
 @pytest.fixture
 def input_facebook_posts_topics_df():
+    row_count = 4
     return pd.DataFrame(
         {
-            "id": ["1", "2", "2"],
-            "url_post_id": ["123", "456", "456"],
-            "topic": ["a", "a", "b"],
-            "has_topic": [True, True, True],
-            "topics": [["a"], ["a", "b"], ["a", "b"]],
-            "has_topics": [True, True, True],
-            "is_economic_labour_tension": [True, True, True],
-            "is_political_tension": [True, True, True],
-            "is_service_related_tension": [True, True, True],
-            "is_community_insecurity_tension": [True, True, True],
-            "is_sectarian_tension": [True, True, True],
-            "is_environmental_tension": [True, True, True],
-            "is_geopolitics_tension": [True, True, True],
-            "is_intercommunity_relations_tension": [True, True, True],
-            "has_tension": [True, True, True],
+            "id": ["1", "2", "2", "3"],
+            "url_post_id": ["123", "456", "456", "789"],
+            "topic": ["a", "a", "b", "non_topic"],
+            "has_topic": [True, True, True, False],
+            "topics": [["a"], ["a", "b"], ["a", "b"], ["non_topic"]],
+            "has_topics": [True, True, True, False],
+            "is_economic_labour_tension": [True] * row_count,
+            "is_political_tension": [True] * row_count,
+            "is_service_related_tension": [True] * row_count,
+            "is_community_insecurity_tension": [True] * row_count,
+            "is_sectarian_tension": [True] * row_count,
+            "is_environmental_tension": [True] * row_count,
+            "is_geopolitics_tension": [True] * row_count,
+            "is_intercommunity_relations_tension": [True] * row_count,
+            "has_tension": [True] * row_count,
         }
     )
 
@@ -54,22 +56,23 @@ def test_inherit_facebook_comment_topics_from_posts(
     input_comments_df, input_facebook_posts_topics_df
 ):
     # Default does not inherit the `topic` or `has_topic` columns
+    row_count = 3
     expected_comments_df = pd.DataFrame(
         {
-            "id": ["1", "2"],
-            "post_id": [123, 456],
-            "topics": [["a"], ["a", "b"]],
-            "has_topics": [True, True],
-            "is_economic_labour_tension": [True, True],
-            "is_political_tension": [True, True],
-            "is_service_related_tension": [True, True],
-            "is_community_insecurity_tension": [True, True],
-            "is_sectarian_tension": [True, True],
-            "is_environmental_tension": [True, True],
-            "is_geopolitics_tension": [True, True],
-            "is_intercommunity_relations_tension": [True, True],
-            "has_tension": [True, True],
-            "comments_only_column": ["some_str", "another_str"],
+            "id": ["1", "2", "3"],
+            "post_id": [123, 456, 789],
+            "topics": [["a"], ["a", "b"], ["non_topic"]],
+            "has_topics": [True, True, False],
+            "is_economic_labour_tension": [True] * row_count,
+            "is_political_tension": [True] * row_count,
+            "is_service_related_tension": [True] * row_count,
+            "is_community_insecurity_tension": [True] * row_count,
+            "is_sectarian_tension": [True] * row_count,
+            "is_environmental_tension": [True] * row_count,
+            "is_geopolitics_tension": [True] * row_count,
+            "is_intercommunity_relations_tension": [True] * row_count,
+            "has_tension": [True] * row_count,
+            "comments_only_column": ["some_str"] * row_count,
         }
     )
 
@@ -84,22 +87,23 @@ def test_inherit_facebook_comment_topics_from_posts_rename(
     input_comments_df, input_facebook_posts_topics_df
 ):
     # Default does not inherit the `topic` or `has_topic` columns
+    row_count = 3
     expected_comments_df = pd.DataFrame(
         {
-            "id": ["1", "2"],
-            "post_id": [123, 456],
-            "classes": [["a"], ["a", "b"]],
-            "has_classes": [True, True],
-            "is_economic_labour_tension": [True, True],
-            "is_political_tension": [True, True],
-            "is_service_related_tension": [True, True],
-            "is_community_insecurity_tension": [True, True],
-            "is_sectarian_tension": [True, True],
-            "is_environmental_tension": [True, True],
-            "is_geopolitics_tension": [True, True],
-            "is_intercommunity_relations_tension": [True, True],
-            "has_tension": [True, True],
-            "comments_only_column": ["some_str", "another_str"],
+            "id": ["1", "2", "3"],
+            "post_id": [123, 456, 789],
+            "classes": [["a"], ["a", "b"], ["non_topic"]],
+            "has_classes": [True, True, False],
+            "is_economic_labour_tension": [True] * row_count,
+            "is_political_tension": [True] * row_count,
+            "is_service_related_tension": [True] * row_count,
+            "is_community_insecurity_tension": [True] * row_count,
+            "is_sectarian_tension": [True] * row_count,
+            "is_environmental_tension": [True] * row_count,
+            "is_geopolitics_tension": [True] * row_count,
+            "is_intercommunity_relations_tension": [True] * row_count,
+            "has_tension": [True] * row_count,
+            "comments_only_column": ["some_str"] * row_count,
         }
     )
 
@@ -113,22 +117,23 @@ def test_inherit_facebook_comment_topics_from_posts_rename(
 def test_inherit_facebook_comment_topics_from_posts_inherit_every_row(
     input_facebook_posts_topics_df, input_comments_df
 ):
+    row_count = 4
     expected_comments_df = pd.DataFrame(
         {
-            "id": ["1", "2", "2"],
-            "post_id": [123, 456, 456],
-            "topics": [["a"], ["a", "b"], ["a", "b"]],
-            "has_topics": [True, True, True],
-            "is_economic_labour_tension": [True, True, True],
-            "is_political_tension": [True, True, True],
-            "is_service_related_tension": [True, True, True],
-            "is_community_insecurity_tension": [True, True, True],
-            "is_sectarian_tension": [True, True, True],
-            "is_environmental_tension": [True, True, True],
-            "is_geopolitics_tension": [True, True, True],
-            "is_intercommunity_relations_tension": [True, True, True],
-            "has_tension": [True, True, True],
-            "comments_only_column": ["some_str", "another_str", "another_str"],
+            "id": ["1", "2", "2", "3"],
+            "post_id": [123, 456, 456, 789],
+            "topics": [["a"], ["a", "b"], ["a", "b"], ["non_topic"]],
+            "has_topics": [True, True, True, False],
+            "is_economic_labour_tension": [True] * row_count,
+            "is_political_tension": [True] * row_count,
+            "is_service_related_tension": [True] * row_count,
+            "is_community_insecurity_tension": [True] * row_count,
+            "is_sectarian_tension": [True] * row_count,
+            "is_environmental_tension": [True] * row_count,
+            "is_geopolitics_tension": [True] * row_count,
+            "is_intercommunity_relations_tension": [True] * row_count,
+            "has_tension": [True] * row_count,
+            "comments_only_column": ["some_str"] * row_count,
         }
     )
 
@@ -142,22 +147,23 @@ def test_inherit_facebook_comment_topics_from_posts_inherit_every_row(
 def test_inherit_facebook_comment_topics_from_posts_inherit_every_row_rename(
     input_facebook_posts_topics_df, input_comments_df
 ):
+    row_count = 4
     expected_comments_df = pd.DataFrame(
         {
-            "id": ["1", "2", "2"],
-            "post_id": [123, 456, 456],
-            "classes": [["a"], ["a", "b"], ["a", "b"]],
-            "has_classes": [True, True, True],
-            "is_economic_labour_tension": [True, True, True],
-            "is_political_tension": [True, True, True],
-            "is_service_related_tension": [True, True, True],
-            "is_community_insecurity_tension": [True, True, True],
-            "is_sectarian_tension": [True, True, True],
-            "is_environmental_tension": [True, True, True],
-            "is_geopolitics_tension": [True, True, True],
-            "is_intercommunity_relations_tension": [True, True, True],
-            "has_tension": [True, True, True],
-            "comments_only_column": ["some_str", "another_str", "another_str"],
+            "id": ["1", "2", "2", "3"],
+            "post_id": [123, 456, 456, 789],
+            "classes": [["a"], ["a", "b"], ["a", "b"], ["non_topic"]],
+            "has_classes": [True, True, True, False],
+            "is_economic_labour_tension": [True] * row_count,
+            "is_political_tension": [True] * row_count,
+            "is_service_related_tension": [True] * row_count,
+            "is_community_insecurity_tension": [True] * row_count,
+            "is_sectarian_tension": [True] * row_count,
+            "is_environmental_tension": [True] * row_count,
+            "is_geopolitics_tension": [True] * row_count,
+            "is_intercommunity_relations_tension": [True] * row_count,
+            "has_tension": [True] * row_count,
+            "comments_only_column": ["some_str"] * row_count,
         }
     )
 
@@ -174,24 +180,25 @@ def test_inherit_facebook_comment_topics_from_posts_inherit_every_row_rename(
 def test_inherit_facebook_comment_topics_from_posts_inherit_every_row_extra_inherited_col(
     input_facebook_posts_topics_df, input_comments_df
 ):
+    row_count = 4
     expected_comments_df = pd.DataFrame(
         {
-            "id": ["1", "2", "2"],
-            "post_id": [123, 456, 456],
-            "topic": ["a", "a", "b"],
-            "has_topic": [True, True, True],
-            "topics": [["a"], ["a", "b"], ["a", "b"]],
-            "has_topics": [True, True, True],
-            "is_economic_labour_tension": [True, True, True],
-            "is_political_tension": [True, True, True],
-            "is_service_related_tension": [True, True, True],
-            "is_community_insecurity_tension": [True, True, True],
-            "is_sectarian_tension": [True, True, True],
-            "is_environmental_tension": [True, True, True],
-            "is_geopolitics_tension": [True, True, True],
-            "is_intercommunity_relations_tension": [True, True, True],
-            "has_tension": [True, True, True],
-            "comments_only_column": ["some_str", "another_str", "another_str"],
+            "id": ["1", "2", "2", "3"],
+            "post_id": [123, 456, 456, 789],
+            "topic": ["a", "a", "b", "non_topic"],
+            "has_topic": [True, True, True, False],
+            "topics": [["a"], ["a", "b"], ["a", "b"], ["non_topic"]],
+            "has_topics": [True, True, True, False],
+            "is_economic_labour_tension": [True] * row_count,
+            "is_political_tension": [True] * row_count,
+            "is_service_related_tension": [True] * row_count,
+            "is_community_insecurity_tension": [True] * row_count,
+            "is_sectarian_tension": [True] * row_count,
+            "is_environmental_tension": [True] * row_count,
+            "is_geopolitics_tension": [True] * row_count,
+            "is_intercommunity_relations_tension": [True] * row_count,
+            "has_tension": [True] * row_count,
+            "comments_only_column": ["some_str"] * row_count,
         }
     )
 
@@ -208,24 +215,25 @@ def test_inherit_facebook_comment_topics_from_posts_inherit_every_row_extra_inhe
 def test_inherit_facebook_comment_topics_from_posts_inherit_every_row_extra_inherited_col_rename(
     input_facebook_posts_topics_df, input_comments_df
 ):
+    row_count = 4
     expected_comments_df = pd.DataFrame(
         {
-            "id": ["1", "2", "2"],
-            "post_id": [123, 456, 456],
-            "class": ["a", "a", "b"],
-            "has_class": [True, True, True],
-            "classes": [["a"], ["a", "b"], ["a", "b"]],
-            "has_classes": [True, True, True],
-            "is_economic_labour_tension": [True, True, True],
-            "is_political_tension": [True, True, True],
-            "is_service_related_tension": [True, True, True],
-            "is_community_insecurity_tension": [True, True, True],
-            "is_sectarian_tension": [True, True, True],
-            "is_environmental_tension": [True, True, True],
-            "is_geopolitics_tension": [True, True, True],
-            "is_intercommunity_relations_tension": [True, True, True],
-            "has_tension": [True, True, True],
-            "comments_only_column": ["some_str", "another_str", "another_str"],
+            "id": ["1", "2", "2", "3"],
+            "post_id": [123, 456, 456, 789],
+            "class": ["a", "a", "b", "non_topic"],
+            "has_class": [True, True, True, False],
+            "classes": [["a"], ["a", "b"], ["a", "b"], ["non_topic"]],
+            "has_classes": [True, True, True, False],
+            "is_economic_labour_tension": [True] * row_count,
+            "is_political_tension": [True] * row_count,
+            "is_service_related_tension": [True] * row_count,
+            "is_community_insecurity_tension": [True] * row_count,
+            "is_sectarian_tension": [True] * row_count,
+            "is_environmental_tension": [True] * row_count,
+            "is_geopolitics_tension": [True] * row_count,
+            "is_intercommunity_relations_tension": [True] * row_count,
+            "has_tension": [True] * row_count,
+            "comments_only_column": ["some_str"] * row_count,
         }
     )
 
