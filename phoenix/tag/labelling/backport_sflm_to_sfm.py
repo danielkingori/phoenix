65353facd1eff0b484138f4ba9c7e6ed14586604
Andrew Sutjahjo <andrewsutjahjo@gmail.com>
2021-12-15 13:36:47 +0100

    Add drop duplicates for SFM

--- phoenix/tag/labelling/backport_sflm_to_sfm.py
+++ phoenix/tag/labelling/backport_sflm_to_sfm.py
@@ -6,5 +6,6 @@ def sflm_to_sfm(sflm_df: pd.DataFrame) -> pd.DataFrame:
     """Transform Single Feature to Label Mapping config to legacy Single Feature Mapping."""
     sflm_df = sflm_df[sflm_df["status"] == "active"]
     sfm_df = sflm_df[["processed_features", "class"]]
+    sfm_df = sfm_df.drop_duplicates()
     sfm_df = sfm_df.rename(columns={"processed_features": "features", "class": "topic"})
     return sfm_df
