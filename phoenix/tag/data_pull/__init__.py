2b01090d5c0918c127549ff7ba105458105d68f9
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-12-23 15:57:50 +0100

    tag/pull_all_data_for_month facebook_posts_pull_json.ipynb

--- phoenix/tag/data_pull/__init__.py
+++ phoenix/tag/data_pull/__init__.py
@@ -1,4 +1,5 @@
 """Tagging (labelling) data pulling (raw data processing) module."""
 from phoenix.tag.data_pull import constants  # noqa: F401
+from phoenix.tag.data_pull import facebook_posts_pull_json  # noqa: F401
 from phoenix.tag.data_pull import run_params  # noqa: F401
 from phoenix.tag.data_pull import youtube_comments_pull  # noqa: F401
