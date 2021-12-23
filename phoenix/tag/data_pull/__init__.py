82d3fa7e6e6a8e7d92e52625699a1f43cb3a8778
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-12-23 16:30:12 +0100

    tag/pull_all_data_for_month youtube_videos_pull_json.ipynb

--- phoenix/tag/data_pull/__init__.py
+++ phoenix/tag/data_pull/__init__.py
@@ -4,3 +4,4 @@ from phoenix.tag.data_pull import facebook_posts_pull_json  # noqa: F401
 from phoenix.tag.data_pull import run_params  # noqa: F401
 from phoenix.tag.data_pull import twitter_pull  # noqa: F401
 from phoenix.tag.data_pull import youtube_comments_pull  # noqa: F401
+from phoenix.tag.data_pull import youtube_videos_pull  # noqa: F401
