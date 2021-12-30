4973146a7d0a15a4e2d6ed772758d73cddb81c2c
Andrew Sutjahjo <andrewsutjahjo@gmail.com>
2021-12-30 11:31:58 +0100

    Add analysis of matches, topics, and features for single_feature_match.py

--- phoenix/tag/topic/single_feature_match.py
+++ phoenix/tag/topic/single_feature_match.py
@@ -1,8 +1,16 @@
 """Single feature match for topic analysis."""
+import logging
+
+import arabic_reshaper
+import matplotlib.pyplot as plt
 import pandas as pd
+from bidi.algorithm import get_display
+from IPython.display import display
 
 
 FILL_TOPIC = "irrelevant"
+
+logger = logging.getLogger(__name__)
 
 
 def get_topics(topic_config, features_df, fill_topic: str = FILL_TOPIC) -> pd.DataFrame:
@@ -38,3 +46,34 @@ def get_topics(topic_config, features_df, fill_topic: str = FILL_TOPIC) -> pd.Da
     topics_df = topics_df.reset_index()
 
     return pd.concat([topics_df, no_topic], ignore_index=True)
+
+
+def get_topics_analysis_df(topics_df: pd.DataFrame) -> pd.DataFrame:
+    """Process the output of get_topics function for analysis purposes."""
+    analysis_df = (
+        topics_df.explode("matched_features")
+        .groupby(["topic", "matched_features"])
+        .size()
+        .reset_index(name="matched_object_count")
+    )
+    analysis_df = analysis_df.sort_values(
+        ["topic", "matched_object_count"], ascending=(True, False)
+    ).reset_index(drop=True)
+    analysis_df["matched_features"] = analysis_df["matched_features"].apply(
+        lambda x: get_display(arabic_reshaper.reshape(x))
+    )
+    return analysis_df
+
+
+def analyse(topics_df: pd.DataFrame) -> pd.DataFrame:
+    """Analyse the outputs of the get_topics function."""
+    analysis_df = get_topics_analysis_df(topics_df)
+    logger.info("Top 3 features per topic:\n")
+    display(analysis_df.groupby("topic").head(3))
+    logger.info("Line bars of distributions of matched features:\n")
+    grouped = analysis_df.groupby("topic")
+    for name, group in grouped:
+        group.plot.bar(x="matched_features", y="matched_object_count")
+        plt.title(str(name), fontsize=15)
+    display(plt.show())
+    return analysis_df
