d1691fe44d96e5c9b907e7354a15edfa3eb0bf27
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2022-01-03 10:41:37 +0100

    tag/pull_all_data_for_month improve of init import

--- phoenix/tag/data_pull/run_params/__init__.py
+++ phoenix/tag/data_pull/run_params/__init__.py
@@ -1,5 +1,2 @@
 """Init of datapull run params."""
-from phoenix.tag.data_pull.run_params import create as create_module
-
-
-create = create_module.create
+from phoenix.tag.data_pull.run_params.general_create import create
