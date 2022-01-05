8a09232ae0a0e04b5274b42e6c0ab593dbd78fdd
dbcerigo <dbcerigo@gmail.com>
2022-01-05 21:53:12 +0000

    Implement (pass through) finalise accounts

---
+++ phoenix/tag/accounts_finalise.py
@@ -0,0 +1,10 @@
+"""Finalise accounts, and text snippets (objects) with account classes (labels) joined to them."""
+import pandas as pd
+
+
+def accounts(object_type: str, accounts_df: pd.DataFrame) -> pd.DataFrame:
+    """Process accounts for object type.
+
+    Currently is just a pass through.
+    """
+    return accounts_df
