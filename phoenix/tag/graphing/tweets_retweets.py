09fc22d9bc732cbe0642711d924c9f79d15ebd2b
dbcerigo <dbcerigo@gmail.com>
2022-01-20 17:31:21 +0000

    Implement class/topic concat for tweets_retweets

--- phoenix/tag/graphing/tweets_retweets.py
+++ phoenix/tag/graphing/tweets_retweets.py
@@ -7,14 +7,35 @@ from phoenix.tag.graphing import phoenix_graphistry, processing_utilities
 
 
 def process(
-    final_tweets: pd.DataFrame, final_accounts: pd.DataFrame
+    final_tweets_classes: pd.DataFrame, final_accounts: pd.DataFrame
 ) -> Tuple[pd.DataFrame, pd.DataFrame]:
     """Process tweets and twitter accounts into retweets edges and nodes data."""
     # edges
-    df = final_tweets[["user_screen_name", "retweeted_user_screen_name"]]
+    edges_classes_df = final_tweets_classes[
+        [
+            "user_screen_name",
+            "retweeted_user_screen_name",
+            "class",
+        ]
+    ]
+    edges_classes_df = processing_utilities.reduce_concat_classes(
+        edges_classes_df, ["user_screen_name", "retweeted_user_screen_name"], "class"
+    )
+    edges_classes_df = edges_classes_df.rename(
+        columns={"user_screen_name": "tweeting_user_screen_name"}
+    )
+
+    df = final_tweets_classes[
+        ["id", "user_screen_name", "retweeted_user_screen_name"]
+    ].drop_duplicates(subset="id", keep="first")
+    df = df[["user_screen_name", "retweeted_user_screen_name"]]
     df = df.groupby(["user_screen_name", "retweeted_user_screen_name"]).size().reset_index()
-    edges_df = df.rename(
-        columns={"user_screen_name": "tweeting_user_screen_name", 0: "times_retweeted"}
+    df = df.rename(columns={"user_screen_name": "tweeting_user_screen_name", 0: "times_retweeted"})
+    edges_df = df.merge(
+        edges_classes_df,
+        how="inner",
+        on=["tweeting_user_screen_name", "retweeted_user_screen_name"],
+        validate="one_to_one",
     )
 
     # nodes
@@ -25,7 +46,7 @@ def process(
     nodes_df = nodes_df.drop_duplicates(ignore_index=True)
     account_labels_df = final_accounts[["object_user_name", "account_label"]]
     account_labels_df = processing_utilities.reduce_concat_classes(
-        account_labels_df, "object_user_name", "account_label"
+        account_labels_df, ["object_user_name"], "account_label"
     )
 
     nodes_df = nodes_df.merge(
--- tests/unit/tag/graphing/test_tweets_retweets.py
+++ tests/unit/tag/graphing/test_tweets_retweets.py
@@ -7,12 +7,14 @@ from phoenix.tag.graphing import tweets_retweets
 
 
 @pytest.fixture
-def input_final_tweets() -> pd.DataFrame:
-    """Input dataframe of tweets."""
+def input_final_tweets_classes() -> pd.DataFrame:
+    """Input dataframe of tweets with classes."""
     return pd.DataFrame(
         {
-            "user_screen_name": ["u_1", "u_2", "u_1", "u_3", "u_3"],
-            "retweeted_user_screen_name": [None, "u_1", "u_2", "u_1", "u_1"],
+            "id": [2, 2, 1, 3, 4, 4, 5],
+            "user_screen_name": ["u_1", "u_1", "u_2", "u_1", "u_3", "u_3", "u_3"],
+            "retweeted_user_screen_name": [None, None, "u_1", "u_2", "u_1", "u_1", "u_1"],
+            "class": ["foo", "foo", "c_1", "c_3", "c_1", "c_3", "c_2"],
         }
     )
 
@@ -36,6 +38,7 @@ def edges() -> pd.DataFrame:
             "tweeting_user_screen_name": ["u_1", "u_2", "u_3"],
             "retweeted_user_screen_name": ["u_2", "u_1", "u_1"],
             "times_retweeted": [1, 1, 2],
+            "class": ["c_3", "c_1", "c_1, c_2, c_3"],
         }
     )
 
@@ -51,10 +54,10 @@ def nodes() -> pd.DataFrame:
     )
 
 
-def test_process(input_final_tweets, input_final_tweets_accounts, edges, nodes):
+def test_process(input_final_tweets_classes, input_final_tweets_accounts, edges, nodes):
     """Test processing inputs to edges and nodes."""
     output_edges, output_nodes = tweets_retweets.process(
-        final_tweets=input_final_tweets, final_accounts=input_final_tweets_accounts
+        final_tweets_classes=input_final_tweets_classes, final_accounts=input_final_tweets_accounts
     )
     pd.testing.assert_frame_equal(edges, output_edges)
     pd.testing.assert_frame_equal(nodes, output_nodes)
