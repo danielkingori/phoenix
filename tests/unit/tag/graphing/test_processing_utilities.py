5727cd9cd8092054f4bf1152d812c9605baaa823
dbcerigo <dbcerigo@gmail.com>
2022-01-20 17:30:36 +0000

    Extend reduce concat class to take multi-index id
    Nice...

--- tests/unit/tag/graphing/test_processing_utilities.py
+++ tests/unit/tag/graphing/test_processing_utilities.py
@@ -21,7 +21,7 @@ def test_reduce_concat_classes():
             "class": ["class_1, class_2", "class_2"],
         }
     )
-    output_df = processing_utilities.reduce_concat_classes(input_df, "id_col", "class")
+    output_df = processing_utilities.reduce_concat_classes(input_df, ["id_col"], "class")
     pd.testing.assert_frame_equal(output_df, expected_df)
 
 
@@ -39,5 +39,29 @@ def test_reduce_concat_classes_no_one_to_one_columns():
             "class": ["class_1, class_2", "class_2"],
         }
     )
-    output_df = processing_utilities.reduce_concat_classes(input_df, "id_col", "class")
+    output_df = processing_utilities.reduce_concat_classes(input_df, ["id_col"], "class")
+    pd.testing.assert_frame_equal(output_df, expected_df)
+
+
+def test_reduce_concat_classes_multi_index_id():
+    """Test reduce and concat classes."""
+    input_df = pd.DataFrame(
+        {
+            "id_col_1": ["id_1", "id_1", "id_1"],
+            "id_col_2": [2, 1, 2],
+            "one_to_one_col": ["val_1", "val_2", "val_1"],
+            "class": ["class_2", "class_2", "class_1"],
+        }
+    )
+    expected_df = pd.DataFrame(
+        {
+            "id_col_1": ["id_1", "id_1"],
+            "id_col_2": [1, 2],
+            "one_to_one_col": ["val_2", "val_1"],
+            "class": ["class_2", "class_1, class_2"],
+        }
+    )
+    output_df = processing_utilities.reduce_concat_classes(
+        input_df, ["id_col_1", "id_col_2"], "class"
+    )
     pd.testing.assert_frame_equal(output_df, expected_df)
