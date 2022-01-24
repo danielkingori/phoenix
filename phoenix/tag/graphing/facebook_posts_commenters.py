50f9590523e00e3e81b21a26e16cb062b55f58cc
dbcerigo <dbcerigo@gmail.com>
2022-01-24 09:47:21 +0300

    Add `type` cols to nodes

--- phoenix/tag/graphing/facebook_posts_commenters.py
+++ phoenix/tag/graphing/facebook_posts_commenters.py
@@ -18,6 +18,7 @@ def process_account_nodes(final_accounts: pd.DataFrame) -> pd.DataFrame:
     df = final_accounts[cols_to_keep]
     df = processing_utilities.reduce_concat_classes(df, ["object_user_name"], "account_label")
     df["node_name"] = df["object_user_name"]
+    df["type"] = "facebook_account"
     return df
 
 
@@ -38,6 +39,7 @@ def process_post_nodes(final_facebook_posts_classes: pd.DataFrame) -> pd.DataFra
     df = final_facebook_posts_classes[cols_to_keep]
     df = processing_utilities.reduce_concat_classes(df, ["object_id"], "class")
     df["node_name"] = df["object_id"]
+    df["type"] = "facebook_post"
     return df
 
 
@@ -47,6 +49,7 @@ def process_commenter_nodes(final_facebook_comments_classes: pd.DataFrame) -> pd
     df = final_facebook_comments_classes[cols_to_keep]
     df = processing_utilities.reduce_concat_classes(df, ["user_name"], "class")
     df["node_name"] = df["user_name"]
+    df["type"] = "facebook_commenter"
     return df
 
 
