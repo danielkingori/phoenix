5f8d36dbf0ae355cb691b25a99c3f89fe0755dec
dbcerigo <dbcerigo@gmail.com>
2022-01-29 10:01:49 +0300

    Implement account_post_commenter to commenter graph

--- phoenix/tag/graphing/processing_utilities.py
+++ phoenix/tag/graphing/processing_utilities.py
@@ -1,5 +1,5 @@
 """Processing utilities."""
-from typing import List
+from typing import List, Tuple
 
 import pandas as pd
 
@@ -34,3 +34,86 @@ def reduce_concat_classes(
         concat_classes_df, how="inner", on=row_id_cols, validate="one_to_one"
     )
     return out_df
+
+
+def account_post_commenter_graph_to_commenter_edges(
+    edges, nodes
+) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """Turn an account-post-commenter graph into a commenter graph."""
+    edges_df = edges[~edges["times_commented"].isna()]
+    edges_df = edges_df.merge(
+        edges_df, how="inner", left_on="destination_node", right_on="destination_node"
+    )
+    edges_df = edges_df[edges_df["source_node_x"] != edges_df["source_node_y"]]
+    edges_df = edges_df.merge(
+        nodes[["node_name", "class"]], how="left", left_on="destination_node", right_on="node_name"
+    )
+    edges_df["joint_num_comments"] = edges_df["times_commented_x"] + edges_df["times_commented_y"]
+    edges_df["edge_key"] = edges_df.apply(
+        lambda row: "-".join(sorted([row["source_node_x"], row["source_node_y"]])), axis=1
+    )
+    edges_df = edges_df[edges_df.duplicated(subset=["edge_key"])]
+    edges_df = edges_df.merge(
+        edges[edges["times_commented"].isna()],
+        how="left",
+        left_on="destination_node",
+        right_on="destination_node",
+    )
+    edges_df = edges_df.merge(
+        nodes[nodes["type"] == "account"][["node_name", "account_label"]],
+        how="left",
+        left_on="source_node",
+        right_on="node_name",
+    )
+    count_df = (
+        edges_df.groupby(["source_node_x", "source_node_y"])[["joint_num_comments"]]
+        .sum()
+        .reset_index()
+    )
+    concat_classes_df = edges_df.groupby(["source_node_x", "source_node_y"])["class"].apply(list)
+    concat_classes_df = concat_classes_df.apply(
+        lambda l: ", ".join(
+            sorted(
+                set(
+                    [
+                        item
+                        for sublist in [classes_string.split(", ") for classes_string in l]
+                        for item in sublist
+                    ]
+                )
+            )
+        )
+    )
+    concat_classes_df = concat_classes_df.reset_index()
+    df = count_df.merge(
+        concat_classes_df, how="left", on=["source_node_x", "source_node_y"], validate="1:1"
+    )
+    concat_account_labels_df = edges_df.groupby(["source_node_x", "source_node_y"])[
+        "account_label"
+    ].apply(list)
+    concat_account_labels_df = concat_account_labels_df.apply(
+        lambda l: ", ".join(
+            sorted(
+                set(
+                    [
+                        item
+                        for sublist in [
+                            classes_string.split(", ")
+                            for classes_string in l
+                            if classes_string and not pd.isnull(classes_string)
+                        ]
+                        for item in sublist
+                    ]
+                )
+            )
+        )
+    )
+    concat_account_labels_df = concat_account_labels_df.reset_index()
+    df = df.merge(
+        concat_account_labels_df, how="left", on=["source_node_x", "source_node_y"], validate="1:1"
+    )
+    df = df.rename(columns={"source_node_x": "source_node", "source_node_y": "destination_node"})
+
+    nodes_df = nodes[nodes["type"] == "commenter"]
+    nodes_df = nodes_df.drop(columns=["type"])
+    return df, nodes_df
