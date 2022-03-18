054d8986dff4d9fc76be280af969318ca1b3c472
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2022-03-18 14:14:37 +0100

    t/cpts DEFAULT_POSTS_TO_SCRAPE_COLUMNS

--- phoenix/tag/export_manual_scraping/__init__.py
+++ phoenix/tag/export_manual_scraping/__init__.py
@@ -3,3 +3,13 @@ from phoenix.tag.export_manual_scraping import filtering, run_params
 
 
 filter_posts = filtering.filter_posts
+
+DEFAULT_POSTS_TO_SCRAPE_COLUMNS = [
+    "phoenix_post_id",
+    "account_name",
+    "post_created",
+    "text",
+    "total_interactions",
+    "post_url",
+    "scrape_url",
+]
