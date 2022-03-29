c9335d62c0ed0191114679ef24ed13ce4f7a92e6
dbcerigo <dbcerigo@gmail.com>
2022-03-29 21:45:24 +0100

    Implement "scraping" fb html posts to crowdtangle

--- phoenix/common/cli_modules/scrape_facebook.py
+++ phoenix/common/cli_modules/scrape_facebook.py
@@ -2,7 +2,7 @@
 import click
 
 from phoenix.common import run_params
-from phoenix.common.cli_modules import scrape_group, utils
+from phoenix.common.cli_modules import comments, scrape_group, tagging, utils
 
 
 @scrape_group.scrape.command("facebook_posts")
@@ -33,6 +33,31 @@ from phoenix.common.cli_modules import scrape_group, utils
     default=None,
     help=("A list of CrowdTangle to override the one set in the tenant." " Format: 'id1,id2'"),
 )
+@click.option(
+    "--process_manually_collected_html_files",
+    is_flag=True,
+    help=(
+        (
+            "Process the manually collected html files of facebook pages into the CrowdTangle "
+            "response format, reading for 'tagging'."
+        )
+    ),
+)
+@click.option(
+    "--max_manually_collected_html_files_to_process",
+    type=click.INT,
+    help="Max number of files to process",
+)
+@click.option(
+    "--silence_no_files_to_process_exception",
+    default=False,
+    type=click.BOOL,
+    help=(
+        "Silence no files to process exception\n"
+        "This is for when the CLI is used in cron jobs so that unnecessary errors aren't created."
+    ),
+)
+@click.argument("month_offset", type=click.INT, default=0)
 def fb(
     artifact_env,
     tenant_id,
@@ -40,11 +65,19 @@ def fb(
     scrape_start_date,
     scrape_end_date,
     crowdtangle_list_ids,
+    process_manually_collected_html_files,
+    max_manually_collected_html_files_to_process,
+    silence_no_files_to_process_exception,
+    month_offset,
 ):
     """Run scrape of facebook posts.
 
     Example command:
     ./phoenix-cli scrape facebook_posts production tenant
+
+    Or, to process posts from manually downloaded html files into CrowdTangle "scrapes":
+    ./phoenix-cli scrape facebook_posts production tenant --process_manually_collected_html_files
+
 
     ARTIFACT_ENV:
         The artifact environment that will be used. Default "local"
@@ -53,8 +86,14 @@ def fb(
     TENANT_ID: The id of the tenant to run phoenix for.
     """
     cur_run_params = run_params.general.create(artifact_env, tenant_id)
+    year_filter, month_filter = utils.get_year_month_for_offset(
+        cur_run_params.run_dt, month_offset
+    )
     extra_parameters = {
         "ARTIFACT_SOURCE_FB_POSTS_URL": cur_run_params.art_url_reg.get_url("source-posts"),
+        "OBJECT_TYPE": "facebook_posts",
+        "YEAR_FILTER": year_filter,
+        "MONTH_FILTER": month_filter,
     }
     parameters = {
         **utils.init_parameters(cur_run_params),
@@ -72,7 +111,33 @@ def fb(
     if crowdtangle_list_ids:
         parameters["CROWDTANGLE_LIST_IDS"] = crowdtangle_list_ids
 
-    input_nb_url = utils.get_input_notebook_path("scrape/fb_post_source_api.ipynb")
-    output_nb_url = cur_run_params.art_url_reg.get_url("source-fb_post_source_api_notebook")
+    if max_manually_collected_html_files_to_process:
+        parameters["MAX_FILES_TO_PROCESS"] = max_manually_collected_html_files_to_process
 
-    utils.run_notebooks(input_nb_url, output_nb_url, parameters)
+    if process_manually_collected_html_files:
+        BASE_URL_FACEBOOK_POSTS_PAGES_TO_PARSE = cur_run_params.art_url_reg.get_url(
+            "base-facebook_posts_pages_to_parse", parameters
+        )
+
+        files_to_process = comments.get_files_to_process(BASE_URL_FACEBOOK_POSTS_PAGES_TO_PARSE)
+        if len(files_to_process) < 1:
+            message = (
+                "There are no files to process in folder: "
+                f"{BASE_URL_FACEBOOK_POSTS_PAGES_TO_PARSE}"
+            )
+            if not silence_no_files_to_process_exception:
+                raise RuntimeError(message)
+            click.echo(message)
+            return
+
+        click.echo("Processing files:")
+        for f in files_to_process:
+            click.echo(f)
+            tagging.tagging_run_notebook(
+                "scrape/facebook_posts_pages_parse.ipynb", parameters, cur_run_params.art_url_reg
+            )
+    else:
+        input_nb_url = utils.get_input_notebook_path("scrape/fb_post_source_api.ipynb")
+        output_nb_url = cur_run_params.art_url_reg.get_url("source-fb_post_source_api_notebook")
+
+        utils.run_notebooks(input_nb_url, output_nb_url, parameters)
