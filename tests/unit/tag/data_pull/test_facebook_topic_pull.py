e90b6cb2e5b2e31ac02cc32ad973cb0f21f6f1e4
Andrew Sutjahjo <andrewsutjahjo@gmail.com>
2023-04-12 16:31:17 +0200

    Add unit test for facebook_topic_pull.get_unique_groups()

--- tests/unit/tag/data_pull/test_facebook_topic_pull.py
+++ tests/unit/tag/data_pull/test_facebook_topic_pull.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import pandas as pd
+import pytest
 
 from phoenix.tag.data_pull import facebook_topic_pull
 
@@ -33,6 +34,40 @@ def test_get_unique_groups_na():
     )
 
     expected_series.index.names = ["user_name"]
+
+    result_series = facebook_topic_pull.get_unique_groups(input_df)
+
+    pd.testing.assert_series_equal(result_series, expected_series)
+
+
+@pytest.mark.parametrize("groupby_key", [("user_name"), ("account_name")])
+def test_get_unique_groups(groupby_key):
+    """Test unique sets of topics are made per group_by key."""
+    input_df = pd.DataFrame(
+        {
+            groupby_key: ["Alice", "Bob", "Bob", "Alice", "Alice", "Alice", "Alice"],
+            "topic": [
+                "apples",
+                "apples",
+                "cherries",
+                "apples",
+                "bananas",
+                "dates",
+                "dates",
+            ],
+        }
+    )
+
+    expected_series = pd.Series(
+        index=["Alice", "Bob"],
+        data=[
+            ["apples", "bananas", "dates"],
+            ["apples", "cherries"],
+        ],
+        name="topic",
+    )
+
+    expected_series.index.names = [groupby_key]
 
     result_series = facebook_topic_pull.get_unique_groups(input_df)
 
